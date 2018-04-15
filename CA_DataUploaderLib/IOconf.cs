c7ad0b1b15862c46d504c24fd5cb15c9cb638e97
Thomas Pedersen <thomas@copenhagenatomics.com>
2018-04-15 21:51:44 +0200

    Let's check the IO.conf settings for simple mistakes.

--- CA_DataUploaderLib/IOconf.cs
+++ CA_DataUploaderLib/IOconf.cs
@@ -14,6 +14,14 @@ namespace CA_DataUploaderLib
             var lines = File.ReadAllLines("IO.conf").ToList();
             lines = lines.Where(x => !x.Trim().StartsWith("//") && x.Trim().Length > 2).Select(x => x.Trim()).ToList();
             Table = lines.Select(x => x.Split(",".ToCharArray()).ToList()).ToList();
+
+            var groups = Table.GroupBy(x => x[0] + x[1]);
+            foreach (var g in groups.Where(x => x.Count() > 1))
+                Console.WriteLine($"WARNING: Name: {g.First()[1]} occure {g.Count()} times in this group: {g.First()[0]}");
+
+            var indexs = Table.Where(x => x.Count() > 2).GroupBy(x => x[2]);
+            foreach (var g in indexs.Where(x => x.Count() > 1))
+                Console.WriteLine($"WARNING: vector index: {g.Key}, occure {g.Count()} times in IO.conf file");
         }
 
         public IEnumerable<List<string>> GetTypes(IOTypes type)
