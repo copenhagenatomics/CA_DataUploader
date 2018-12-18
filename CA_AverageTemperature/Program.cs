f2b20ef72e344e93888a3281a6da0d4d89e06683
Thomas Jam Pedersen <thomas@copenhagenatomics.com>
2018-12-18 20:37:49 +0100

    Better debugging.

--- CA_AverageTemperature/Program.cs
+++ CA_AverageTemperature/Program.cs
@@ -14,9 +14,22 @@ namespace CA_AverageTemperature
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (Exception e)
+            {
+                if (e.ToString().Contains("Could not load file or assembly 'Microsoft.VisualBasic"))
+                    Console.WriteLine("Please install: sudo apt-get install mono-vbnc");
+
+                if (e.InnerException == null)
+                    MessageBox.Show(e.ToString());
+                else
+                    MessageBox.Show(e.InnerException.ToString());
+            }
         }
     }
 }
