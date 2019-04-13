0c9d8238a2714037cfe0d78e9e1c8befb9f19933
Thomas Pedersen <thomas@copenhagenatomics.com>
2019-04-13 12:36:31 +0200

    Added small app to show available MCU boards.

---
+++ ShowAvailableMCUboards/Program.cs
@@ -0,0 +1,25 @@
+﻿using CA_DataUploaderLib;
+using System;
+
+namespace ShowAvailableMCUboards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ports = new SerialNumberMapper(true);
+
+            foreach(var board in ports.McuBoards)
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine(board.ToString(Environment.NewLine));
+
+                if(board.IsOpen)
+                {
+                    for(int i=0; i<4; i++)
+                        Console.WriteLine(board.ReadLine());
+                }
+            }
+        }
+    }
+}
