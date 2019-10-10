7e9781fcb4182782480bf6a40100dc068692e164
Thomas Pedersen <thomas@copenhagenatomics.com>
2019-10-10 14:22:02 +0200

    minor change in scall test.

--- UnitTests/SerialPortTests.cs
+++ UnitTests/SerialPortTests.cs
@@ -11,12 +11,13 @@ namespace UnitTests
         [TestMethod]
         public void Scale()
         {
-            Console.WriteLine("Set the scale in RS232 mode by holding MODE until display show Prt, then press TARA. ");
-            Console.WriteLine("Then press MODE until you see 'Cont in', then press TARA. Then connect to COM port with 9600 baud");
+            //Set the scale in RS232 mode by holding MODE until display show Prt, then press TARA. 
+            //Then press MODE until you see 'Cont in', then press TARA. Then connect to COM port with 9600 baud.
+
             var mapper = new SerialNumberMapper(true);
             var list = mapper.ByFamily("Scale");
 
-            for(int i=0; i<100; i++)
+            for(int i=0; i<50; i++)
             {
                 string str = string.Empty;
                 foreach(var scale in list)
