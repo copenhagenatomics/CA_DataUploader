254a153655e2048aa1bbccb52c6ce50b5e26eac1
Thomas Jam Pedersen <thomas@copenhagenatomics.com>
2020-01-05 14:28:00 +0100

    Able to handle fake data.

--- CA_AverageTemperature/JackRectangle.cs
+++ CA_AverageTemperature/JackRectangle.cs
@@ -12,26 +12,31 @@ namespace CA_AverageTemperature
         public Point TitlePos;
         public Point SerialPos;
 
-        public JackRectangle(SensorSample sensor, double width, double height)
+        public JackRectangle(SensorSample sensor, double colWidth, double lineHeight)
         {
+            double height = lineHeight / 16.0;
+            double width = sensor.NumberOfPorts == "" ? colWidth / 10.5 : colWidth / 13;
+
             Sensor = sensor;
-            int x = 0, y = 0;
+            int x = 0, y = 0, w = 9;
             switch (sensor.NumberOfPorts)
             {
                 case "2x8":
                     x = (int)(((16 - sensor.Jack) % 8 + 0.5) * width);
                     y = (int)(((sensor.Hub) * 4 + ((sensor.Jack - 1) / 8) + 1) * height);
+                    w = 9;
                     break;
                 default:
                 case "1x10":
                     x = (int)(((10 - sensor.Jack) + 0.5) * width); 
                     y = (int)((sensor.Hub) * 4 + height);   // Wynand please fix. 
+                    w = 11;
                     break;
             }
             Rect = new Rectangle(x, y, (int)width, (int)height);
             if(sensor.Jack == 0)
             {
-                Rect = new Rectangle((int)(9 * width), (int)(y + height / 2), (int)width, (int)height);
+                Rect = new Rectangle((int)(w * width), (int)(y + height / 2), (int)width, (int)height);
                 TitlePos = Rect.Location;
                 TitlePos.Y -= Rect.Height / 2;
                 TitlePos.X += Rect.Width / 2;
