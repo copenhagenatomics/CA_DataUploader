a8eee27e88270eac6c5e7adef071905d8b57c60e
Thomas Jam Pedersen <thomas@copenhagenatomics.com>
2020-03-11 20:43:03 +0100

    Ups, wrong file.

--- CA_DataUploaderLib/IOconf/IOconfSaltLeakage.cs
+++ CA_DataUploaderLib/IOconf/IOconfSaltLeakage.cs
@@ -10,9 +10,8 @@ namespace CA_DataUploaderLib.IOconf
             Name = list[1];
             BoxName = list[2];
             SetMap(BoxName);
-            if (!int.TryParse(list[3], out PortNumber)) throw new Exception("IOconfTypeK: wrong port number: " + row);
-            if (PortNumber < 0 || PortNumber > 16) throw new Exception("IOconfTypeK: invalid port number: " + row);
-
+            if (!int.TryParse(list[3], out PortNumber)) throw new Exception("IOconfSaltLeakage: wrong port number: " + row);
+            if (PortNumber < 1 || PortNumber > 16) throw new Exception("IOconfSaltLeakage: invalid port number: " + row);
         }
 
     }
