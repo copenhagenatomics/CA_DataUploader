1631d69070dd07feb1bc0143ca7ac957b394367a
Thomas Pedersen <thomas@copenhagenatomics.com>
2020-05-31 16:12:39 +0200

    Remove some error message.

--- CA_DataUploaderLib/SwitchBoardBase.cs
+++ CA_DataUploaderLib/SwitchBoardBase.cs
@@ -9,8 +9,8 @@ namespace CA_DataUploaderLib
     public static class SwitchBoardBase
     {
         private const string _SwitchBoxPattern = "P1=(\\d\\.\\d\\d)A P2=(\\d\\.\\d\\d)A P3=(\\d\\.\\d\\d)A P4=(\\d\\.\\d\\d)A";
-        private static Match _LatestRead;
-        private static DateTime _lastTimeStamp;
+        private static Match _LatestRead = null;
+        private static DateTime _lastTimeStamp = DateTime.MinValue;
 
         public static List<double> ReadInputFromSwitchBoxes(MCUBoard box)
         {
@@ -23,32 +23,36 @@ namespace CA_DataUploaderLib
                 // see if it matches the BoxPattern.
                 var match = Regex.Match(lines, _SwitchBoxPattern);
 
-                // if match, then store this value for later unsucessful reads. 
-                if (match.Success)
+                lock (_SwitchBoxPattern)
                 {
-                    _LatestRead = match;
-                    _lastTimeStamp = DateTime.Now;
-                }
-                else
-                {
-                    if (_lastTimeStamp.AddSeconds(2) > DateTime.Now) // if it is less than 2 seconds old, then return last read. 
-                        match = _LatestRead;
+                    // if match, then store this value for later unsucessful reads. 
+                    if (match.Success && match.Groups.Count > 4)
+                    {
+                        _LatestRead = match;
+                        _lastTimeStamp = DateTime.Now;
+                    }
+                    else
+                    {
+                        if (_LatestRead == null)
+                            return new List<double>();
+
+                        if (_lastTimeStamp.AddSeconds(2) > DateTime.Now) // if it is less than 2 seconds old, then return last read. 
+                            match = _LatestRead;
+                    }
                 }
 
-                double dummy;
-                if (match.Success)
+                if (match.Success && match.Groups.Count > 4)
                 {
                     return match.Groups.Cast<Group>().Skip(1)
-                        .Where(x => double.TryParse(x.Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out dummy))
                         .Select(x => double.Parse(x.Value, CultureInfo.InvariantCulture)).ToList();
                 }
             }
             catch(Exception ex)
             {
-                CALog.LogException(LogID.A, ex);
+                CALog.LogException(LogID.B, ex);
             }
 
-            CALog.LogData(LogID.B, "ReadInputFromSwitchBoxes: " + lines + Environment.NewLine);
+            CALog.LogData(LogID.B, $"ReadInputFromSwitchBoxes: '{lines}'{Environment.NewLine}");
             return new List<double>();  // empty list
         }
     }
