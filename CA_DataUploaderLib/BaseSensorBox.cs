8964c7d61738b234252c905bc8308d39a2667538
freddyrios <freddy.rios@copenhagenatomics.com>
2021-02-08 17:06:00 +0100

    diff issue - extra time for error retries on BaseSensorBox

--- CA_DataUploaderLib/BaseSensorBox.cs
+++ CA_DataUploaderLib/BaseSensorBox.cs
@@ -73,11 +73,12 @@ namespace CA_DataUploaderLib
 
             while (_running)
             {
+                Thread.Sleep(100); //boards typically write a line every 100 ms (note we also way on exception cases)
+
                 try
                 {
                     ReadSensors();
                     CheckFails(); // check if any of the boards stopped responding. 
-                    Thread.Sleep(100);//boards typically write a line every 100 ms
                 }
                 catch (Exception ex)
                 {
