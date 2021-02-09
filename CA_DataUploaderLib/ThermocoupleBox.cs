406bfcc0a30dd03c3bcb58a3bd7ba99e769e9c0f
freddyrios <freddy.rios@copenhagenatomics.com>
2021-02-09 14:53:05 +0100

    removed code no longer in use

--- CA_DataUploaderLib/ThermocoupleBox.cs
+++ CA_DataUploaderLib/ThermocoupleBox.cs
@@ -1,11 +1,9 @@
 ﻿using CA_DataUploaderLib.IOconf;
 using CA_DataUploaderLib.Extensions;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using CA_DataUploaderLib.Helpers;
-using System.Diagnostics;
 
 namespace CA_DataUploaderLib
 {
@@ -13,7 +11,6 @@ namespace CA_DataUploaderLib
     {
         private readonly SensorSample _rpiGpuSample;
         private readonly SensorSample _rpiCpuSample;
-        private readonly Stopwatch initDelayWatch;
         public ThermocoupleBox(CommandHandler cmd)
         {
             Title = "Thermocouples";
@@ -48,7 +45,6 @@ namespace CA_DataUploaderLib
                     board.WriteLine("Junction");
             }
 
-            initDelayWatch = Stopwatch.StartNew();
             new Thread(() => this.LoopForever()).Start();
         }
 
