b635325f6b2a9445f5f91bb2b26bede22dc5516b
freddyrios <freddy.rios@copenhagenatomics.com>
2021-02-19 09:41:58 +0100

    .net 5 regression: child parallel processes crash

--- CA_DataUploaderLib/Helpers/DULutil.cs
+++ CA_DataUploaderLib/Helpers/DULutil.cs
@@ -54,7 +54,7 @@ namespace CA_DataUploaderLib.Helpers
             lock (SingleProcessLock)
             {
                 if (timeSinceLastProcessExit.IsRunning && timeSinceLastProcessExit.ElapsedMilliseconds < 10)
-                    Thread.Sleep(10);
+                    Thread.Sleep(20);
 
                 using (var p = Process.Start(info))
                 {
