272818ab6854c401650e97fb06a580316d338555
freddyrios <freddy.rios@copenhagenatomics.com>
2021-07-23 11:12:51 +0200

    Revert "Merge pull request #94 from copenhagenatomics/bug/write-timeouts"
    This reverts commit 21220605f6603d0393a4cb950c30a0c6bf3509fb, reversing
    changes made to 9a11b586e86954e3ea4482dab52bf7a3eeccb463.

--- CA_DataUploaderLib/MCUBoard.cs
+++ CA_DataUploaderLib/MCUBoard.cs
@@ -54,7 +54,9 @@ namespace CA_DataUploaderLib
         private static readonly Regex _scaleRegex = new Regex("[+-](([0-9]*[.])?[0-9]+) kg"); // "+0000.00 kg"
         private static int _detectedScaleBoards;
         private static int _detectedUnknownBoards;
-        private SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
+        // the "writer" for this lock are operations that close/reopens the connection, while the readers are any other operation including SafeWriteLine.
+        // This prevents operations being ran when the connections are being closed/reopened.
+        private AsyncReaderWriterLock _reconnectionLock = new AsyncReaderWriterLock();
 
         public BoardSettings ConfigSettings { get; set; } = BoardSettings.Default;
 
@@ -128,7 +130,7 @@ namespace CA_DataUploaderLib
 
         public async Task SafeClose(CancellationToken token)
         {
-            await _lock.WaitAsync(token);
+            await _reconnectionLock.AcquireWriterLock(token);
             try
             {
                 if (IsOpen)
@@ -136,7 +138,7 @@ namespace CA_DataUploaderLib
             }
             finally
             {
-                _lock.Release();
+                _reconnectionLock.ReleaseWriterLock();
             }
         }
 
@@ -161,7 +163,7 @@ namespace CA_DataUploaderLib
         {
             var lines = new List<string>();
             var bytesToRead500ms = 0;
-            await _lock.WaitAsync(token);
+            await _reconnectionLock.AcquireWriterLock(token);
             try
             {
                 if (IsOpen)
@@ -189,7 +191,7 @@ namespace CA_DataUploaderLib
             }
             finally
             {
-                _lock.Release();
+                _reconnectionLock.ReleaseWriterLock();
             }
 
             CALog.LogData(LogID.B, $"Reopened port {PortName} {productType} {serialNumber} - {bytesToRead500ms} bytes in read buffer.{Environment.NewLine}Skipped header lines '{string.Join("§", lines)}'");
@@ -328,14 +330,14 @@ namespace CA_DataUploaderLib
 
         private async Task<TResult> RunEnsuringConnectionIsOpen<TResult>(string actionName, Func<TResult> action, CancellationToken token)
         {
-            await _lock.WaitAsync(token);
+            await _reconnectionLock.AcquireReaderLock(token);
             try
             {
                 return action(); // the built actions like ReadLine, etc are documented to throw InvalidOperationException if the connection is not opened.                
             }
             finally
             {
-                _lock.Release();
+                _reconnectionLock.ReleaseReaderLock();
             }
         }
 
