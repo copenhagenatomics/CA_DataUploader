01da2b3527048b71934a2d6267520019a2042fd2
freddyrios <freddy.rios@copenhagenatomics.com>
2021-09-15 20:52:11 +0200

    added unit test for SetCalibrationAtIndex

--- UnitTests/BoardSettingsTests.cs
+++ UnitTests/BoardSettingsTests.cs
@@ -12,5 +12,14 @@ namespace UnitTests
             var parser = BoardSettings.Default.Parser;
             CollectionAssert.AreEqual(new[] {0.01,0.05,0.06}, parser.TryParseAsDoubleList("0.01,0.05,0.06"));
         }
+        
+        [TestMethod]
+        public void CanSetCalibrationAtIndex()
+        {
+            var settings = new BoardSettings();
+            settings.SetCalibrationAtIndex("SSSSSSF", 'G', 1);
+            settings.SetCalibrationAtIndex("SSSSSSF", 'M', 3);
+            Assert.AreEqual("SGSMSSF", settings.Calibration);
+        }
     }
 }
