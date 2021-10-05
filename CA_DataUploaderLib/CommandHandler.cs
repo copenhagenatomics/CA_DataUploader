copenhagenatomics/CA_DataUploader

commit 9e1fae4193633354333694cca50eb942907c8d34

accepted user commands and alerts are now registered as events (#127)
* accepted user commands and alerts are now registered as events

By registering these low frequency events it will be possible later to check relevant actions and alerts that have been triggered for the system

Also:
* fixed bug in multi command alerts
* CommandHandler.AlertFired has been removed, use CommandHandler.EventFired and check type == EventType.Alert instead
* fixed double firing of command events

CA_DataUploaderLib/CommandHandler.cs
@@ -25,7 +25,7 @@ namespace CA_DataUploaderLib
         private Lazy<VectorFilterAndMath> _fullsystemFilterAndMath;
 
         public event EventHandler<NewVectorReceivedArgs> NewVectorReceived;
-        public event EventHandler<AlertFiredArgs> AlertFired;
+        public event EventHandler<EventFiredArgs> EventFired;
         public bool IsRunning { get { return _running; } }
 
         public CommandHandler(SerialNumberMapper mapper = null)
@@ -99,11 +99,12 @@ namespace CA_DataUploaderLib
 
         public void OnNewVectorReceived(IEnumerable<SensorSample> vector) =>
             NewVectorReceived?.Invoke(this, new NewVectorReceivedArgs(vector.ToDictionary(v => v.Name, v => v.Value)));
-        
-        public void FireAlert(string msg)
-        {
-            AlertFired?.Invoke(this, new AlertFiredArgs(msg));
-        }
+
+        public void FireAlert(string msg) => FireAlert(msg, DateTime.UtcNow);
+        public void FireAlert(string msg, DateTime timespan) => FireCustomEvent(msg, timespan, (byte)EventType.Alert);
+        /// <summary>registers a custom event (low frequency, such like user commands and alerts that have a max firing rate)</summary>
+        /// <remarks>preferably use values above 100 for eventType to avoid future collisions with built in event types</remarks>
+        public void FireCustomEvent(string msg, DateTime timespan, byte eventType) => EventFired?.Invoke(this, new EventFiredArgs(msg, eventType, timespan));
         private bool Stop(List<string> args)
         {
             _running = false;
@@ -168,14 +169,18 @@ namespace CA_DataUploaderLib
         private List<bool> RunCommandFunctions(string cmdString, bool addToCommandHistory, List<string> cmd, List<Func<List<string>, bool>> commandFunctions)
         {
             List<bool> executionResults = new List<bool>(commandFunctions.Count);
+            var isFirstAccepted = true;
             foreach (var func in commandFunctions)
             {
                 try
                 {
                     bool accepted;
                     executionResults.Add(accepted = func.Invoke(cmd));
-                    if (accepted)
+                    if (accepted && isFirstAccepted)
+                    {//avoid unnecesarily trying to add the command multiple times + triggering the command's EventFired
+                        isFirstAccepted = false;
                         OnCommandAccepted(cmdString, addToCommandHistory); // track it in the history if at least one execution accepted the command
+                    }
                     else
                         break; // avoid running the command on another subsystem when it was already rejected
                 }
@@ -208,6 +213,7 @@ namespace CA_DataUploaderLib
             if (AcceptedCommands.LastOrDefault() != cmdString)
                 AcceptedCommands.Add(cmdString);
             AcceptedCommandsIndex = AcceptedCommands.Count;
+            FireCustomEvent(cmdString, DateTime.UtcNow, (byte)EventType.Command);
         }
 
         private string GetCommand()

CA_DataUploaderLib/Alerts.cs
@@ -72,7 +72,7 @@ namespace CA_DataUploaderLib
 
         public override void OnNewVectorReceived(object sender, NewVectorReceivedArgs e)
         {
-            var timestamp = e.GetVectorTime().ToString("yyyy.MM.dd HH:mm:ss");
+            var timestamp = e.GetVectorTime();
             var (alertsToTrigger, noSensorAlerts) = GetAlertsToTrigger(e); // we gather alerts separately from triggering, to reduce time locking the _alerts list
             
             foreach (var a in alertsToTrigger ?? Enumerable.Empty<IOconfAlert>())
@@ -104,17 +104,16 @@ namespace CA_DataUploaderLib
                 noSensorAlerts ?? Enumerable.Empty<IOconfAlert>());
         }
 
-        private void TriggerAlert(IOconfAlert a, string timestamp, string message)
+        private void TriggerAlert(IOconfAlert a, DateTime timestamp, string message)
         {
-            message = timestamp + message;
-            logger.LogError(message);
+            logger.LogError(timestamp.ToString("yyyy.MM.dd HH:mm:ss") + message);
             if (a.Command != default)
             {
                 foreach (var commands in a.Command.Split('|'))
-                    ExecuteCommand(a.Command);
+                    ExecuteCommand(commands);
             }
 
-            _cmd.FireAlert(message);            
+            _cmd.FireAlert(message, timestamp);            
         }
 
         private List<T> EnsureInitialized<T>(ref List<T> list) => list = list ?? new List<T>();

CA_DataUploaderLib/EventFiredArgs.cs
@@ -1,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CA_DataUploaderLib
+{
+    public class EventFiredArgs : EventArgs
+    {
+        public EventFiredArgs(string data, EventType eventType, DateTime timespan) : this(data, (byte) eventType, timespan) { }
+        public EventFiredArgs(string data, byte eventType, DateTime timespan)
+        {
+            Data = data;
+            EventType = eventType;
+            TimeSpan = timespan;
+        }
+
+        public string Data { get; }
+        public byte EventType { get; }
+        public DateTime TimeSpan { get; }
+
+        public byte[] ToByteArray()
+        {
+            var timeTicks = TimeSpan.Ticks;
+            var encoding = Encoding.UTF8;
+            var dataBytesCount = encoding.GetByteCount(Data);
+            var bytes = new byte[1 + 1 + sizeof(long) + dataBytesCount]; //version + event type + time ticks + data bytes
+            bytes[0] = 0;
+            bytes[1] = EventType;
+            MemoryMarshal.Write(bytes.AsSpan()[2..], ref timeTicks);
+            var startOfDataIndex = 1 + 1 + sizeof(long);
+            if (dataBytesCount != encoding.GetBytes(Data, 0, Data.Length, bytes, startOfDataIndex))
+                throw new InvalidOperationException($"unexpected error getting utf8 bytes for event: {Data}");
+            return bytes;
+        }
+    }
+}

CA_DataUploaderLib/EventType.cs
@@ -1,0 +1,8 @@
+﻿namespace CA_DataUploaderLib
+{
+    public enum EventType : byte
+    {
+        Alert = 0,
+        Command = 1
+    }
+}

CA_DataUploaderLib/ServerUploader.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -20,7 +19,7 @@ namespace CA_DataUploaderLib
         private readonly PlotConnection _plot;
         private readonly Signing _signing;
         private readonly Queue<DataVector> _queue = new Queue<DataVector>();
-        private readonly Queue<string> _alertQueue = new Queue<string>();
+        private readonly Queue<EventFiredArgs> _eventsQueue = new Queue<EventFiredArgs>();
         private readonly List<DateTime> _badPackages = new List<DateTime>();
         private DateTime _lastTimestamp;
         private bool _running;
@@ -47,7 +46,7 @@ namespace CA_DataUploaderLib
                 _cmd = cmd;
                 cmd?.AddCommand("escape", Stop);
                 if (cmd != null)
-                    cmd.AlertFired += SendAlert;
+                    cmd.EventFired += SendEvent;
             }
             catch (Exception ex)
             {
@@ -56,12 +55,11 @@ namespace CA_DataUploaderLib
             }
         }
 
-        private void SendAlert(object sender, AlertFiredArgs e) => SendAlert(e.Message);
-        internal void SendAlert(string message)
+        private void SendEvent(object sender, EventFiredArgs e)
         {
-            lock (_alertQueue)
-                if (_alertQueue.Count < 10000)  // if sending thread can't catch up, then drop packages.
-                    _alertQueue.Enqueue(message);
+            lock (_eventsQueue)
+                if (_eventsQueue.Count < 10000)  // if sending thread can't catch up, then drop packages.
+                    _eventsQueue.Enqueue(e);
         }
 
         public void SendVector(List<double> vector, DateTime timestamp)
@@ -103,11 +101,11 @@ namespace CA_DataUploaderLib
                     if (list != null)
                         PostVectorAsync(GetSignedVectors(list), list.First().timestamp);
 
-                    var alerts = DequeueAllEntries(_alertQueue);
-                    if (alerts != null)
+                    var events = DequeueAllEntries(_eventsQueue);
+                    if (events != null)
                     {
-                        foreach (var alert in alerts)
-                            PostAlertAsync(alert);
+                        foreach (var @event in events)
+                            PostEventAsync(@event);
                     }
 
                     PrintBadPackagesMessage(false);
@@ -164,10 +162,10 @@ namespace CA_DataUploaderLib
             return SignAndCompress(listLen.Concat(theData).ToArray());
         }
 
-        private byte[] SignedMessage(string message)
-        {
-            var buffer = Encoding.UTF8.GetBytes(message);
-            return _signing.GetSignature(buffer).Concat(buffer).ToArray();
+        private byte[] GetSignedEvent(EventFiredArgs @event)
+        { //this can be made more efficient to avoid extra allocations and/or use a memory pool, but these are for low frequency events so postponing looking at that.
+            var bytes = @event.ToByteArray();
+            return _signing.GetSignature(bytes).Concat(bytes).ToArray(); 
         }
 
         private static void OnError(string message, Exception ex) => CALog.LogErrorAndConsoleLn(LogID.A, message, ex);
@@ -188,11 +186,11 @@ namespace CA_DataUploaderLib
             }
         }
 
-        private async void PostAlertAsync(string message)
+        private async void PostEventAsync(EventFiredArgs args)
         {
             try
             {
-                await _plot.PostAlertAsync(SignedMessage(message));
+                await _plot.PostEventAsync(GetSignedEvent(args));
             }
             catch (Exception ex)
             {
@@ -200,7 +198,8 @@ namespace CA_DataUploaderLib
                 {
                     _badPackages.Add(DateTime.UtcNow);
                 }
-                OnError("failed posting alert: " + message, ex);
+
+                OnError($"failed posting event: {args.EventType} - {args.Data} - {args.TimeSpan}", ex);
             }
         }
 
@@ -252,9 +251,9 @@ namespace CA_DataUploaderLib
                 response.EnsureSuccessStatusCode();
             }
 
-            public async Task PostAlertAsync(byte[] signedMessage)
+            public async Task PostEventAsync(byte[] signedMessage)
             {
-                string query = $"api/LoopApi/AlertMessage?plotnameID={_plotID}";
+                string query = $"api/LoopApi/LogEvent?plotnameID={_plotID}";
                 var response = await _client.PutAsJsonAsync(query, signedMessage);
                 response.EnsureSuccessStatusCode();
             }

UnitTests/EventFiredArgsTests.cs
@@ -1,0 +1,24 @@
+﻿using CA_DataUploaderLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EventFiredArgsTests
+    {
+        [TestMethod]
+        public void ToByteArrayReturnsExpectedBytes()
+        {
+            const string Data = "my message";
+            DateTime timespan = new DateTime(2012, 1, 1, 1, 1, 1, 1, DateTimeKind.Utc);
+            var args = new EventFiredArgs(Data, 2, timespan);
+            var bytes = args.ToByteArray();
+            CollectionAssert.AreEqual(new byte[] { 0, 2 }, bytes.Take(2).ToList());
+            CollectionAssert.AreEqual(BitConverter.GetBytes(timespan.Ticks), bytes.Skip(2).Take(sizeof(long)).ToList());
+            CollectionAssert.AreEqual(Encoding.UTF8.GetBytes(Data), bytes.Skip(2).Skip(sizeof(long)).ToList());
+        }
+    }
+}

commit 250ab1f059f904329083338785ec05c29ead8469

allow using in applications not published as a single file (#128)
* breaking change: plugins run from the "plugins" subfolder

reason: this allows to run without publishing the app as a single file, which at the time of writing allows to workaround https://github.com/dotnet/runtime/issues/59961

* Also fixed regression in #127 preventing the full help from showing in the help command.

CA_DataUploaderLib/CommandHandler.cs
@@ -181,7 +181,7 @@ namespace CA_DataUploaderLib
                         isFirstAccepted = false;
                         OnCommandAccepted(cmdString, addToCommandHistory); // track it in the history if at least one execution accepted the command
                     }
-                    else
+                    else if (!accepted)
                         break; // avoid running the command on another subsystem when it was already rejected
                 }
                 catch (ArgumentException ex)

CA_DataUploaderLib/PluginsLoader.cs
@@ -12,6 +12,7 @@ namespace CA_DataUploaderLib
 {
     public class PluginsLoader
     { // see https://docs.microsoft.com/en-us/dotnet/core/tutorials/creating-app-with-plugin-support
+        private readonly string targetFolder = "plugins";
         readonly Dictionary<string, (AssemblyLoadContext ctx, IEnumerable<LoopControlCommand> instances)> _runningPlugins =
             new Dictionary<string, (AssemblyLoadContext ctx, IEnumerable<LoopControlCommand> instances)>();
         SingleFireFileWatcher _pluginChangesWatcher;
@@ -19,24 +20,26 @@ namespace CA_DataUploaderLib
         readonly CommandHandler handler;
         UpdatePluginsCommand updatePluginsCommand;
 
-        public PluginsLoader(CommandHandler handler, Func<(string pluginName, string targetFolder), Task> pluginDownloader = null)
+        public PluginsLoader(CommandHandler handler, Func<(string pluginName, string targetFolder), Task> pluginDownloader = null, string targetFolder = "plugins")
         {
             this.handler = handler;
+            this.targetFolder = targetFolder;
             if (pluginDownloader != null)
-                this.updatePluginsCommand = new UpdatePluginsCommand(handler, pluginDownloader, this);
+                this.updatePluginsCommand = new UpdatePluginsCommand(handler, pluginDownloader, this, targetFolder);
         }
 
         public IEnumerable<string> GetRunningPluginsNames() => _runningPlugins.Keys.Select(v => Path.GetFileNameWithoutExtension(v));
 
         public void LoadPlugins(bool automaticallyLoadPluginChanges = true)
         {
+            Directory.CreateDirectory(targetFolder);
             // load all
-            foreach (var assembly in Directory.GetFiles(".", "*.dll"))
-                LoadPlugin(assembly);
+            foreach (var assemblyFullPath in Directory.GetFiles(targetFolder, "*.dll"))
+                LoadPlugin(assemblyFullPath);
 
             if (automaticallyLoadPluginChanges)
             {
-                _pluginChangesWatcher = new SingleFireFileWatcher(".", "*.dll");
+                _pluginChangesWatcher = new SingleFireFileWatcher(targetFolder, "*.dll");
                 _pluginChangesWatcher.Deleted += UnloadPlugin;
                 _pluginChangesWatcher.Changed += ReloadPlugin;
             }
@@ -49,10 +52,9 @@ namespace CA_DataUploaderLib
             GC.Collect(); // triggers the unload of the assembly (after DoUnloadExtension we no longer have references to the instances)
         }
 
-        void LoadPlugin(string assemblyPath)
+        void LoadPlugin(string assemblyFullPath)
         {
-            assemblyPath = Path.GetFullPath(assemblyPath);
-            var (context, plugins) = Load(assemblyPath, plugingArgs);
+            var (context, plugins) = Load(assemblyFullPath, plugingArgs);
             var initializedPlugins = plugins.ToList();
             if (initializedPlugins.Count == 0)
             {
@@ -63,26 +65,25 @@ namespace CA_DataUploaderLib
             foreach (var plugin in initializedPlugins)
                 plugin.Initialize(new PluginsCommandHandler(handler), new PluginsLogger(plugin.Name)); 
 
-            _runningPlugins[assemblyPath] = (context, initializedPlugins);
-            CALog.LogData(LogID.A, $"loaded plugins from {assemblyPath} - {string.Join(",", initializedPlugins.Select(e => e.GetType().Name))}");
+            _runningPlugins[assemblyFullPath] = (context, initializedPlugins);
+            CALog.LogData(LogID.A, $"loaded plugins from {assemblyFullPath} - {string.Join(",", initializedPlugins.Select(e => e.GetType().Name))}");
         }
 
-        void UnloadPlugin(string assemblyPath)
+        void UnloadPlugin(string assemblyFullPath)
         {
-            assemblyPath = Path.GetFullPath(assemblyPath);
-            if (!_runningPlugins.TryGetValue(assemblyPath, out var runningPluginEntry))
-                CALog.LogData(LogID.A, $"running plugin not found: {Path.GetFileNameWithoutExtension(assemblyPath)}");
+            if (!_runningPlugins.TryGetValue(assemblyFullPath, out var runningPluginEntry))
+                CALog.LogData(LogID.A, $"running plugin not found: {Path.GetFileNameWithoutExtension(assemblyFullPath)}");
             else
-                UnloadPlugin(assemblyPath, runningPluginEntry);
+                UnloadPlugin(assemblyFullPath, runningPluginEntry);
         }
 
-        void UnloadPlugin(string assemblyPath, (AssemblyLoadContext ctx, IEnumerable<LoopControlCommand> instances) entry)
+        void UnloadPlugin(string assemblyFullPath, (AssemblyLoadContext ctx, IEnumerable<LoopControlCommand> instances) entry)
         {
             foreach (var instance in entry.instances)
                 instance.Dispose();
-            _runningPlugins.Remove(assemblyPath);
+            _runningPlugins.Remove(assemblyFullPath);
             entry.ctx.Unload();
-            CALog.LogData(LogID.A, $"unloaded plugins from {assemblyPath}");
+            CALog.LogData(LogID.A, $"unloaded plugins from {assemblyFullPath}");
         }
 
         /// <remarks>
@@ -94,16 +95,16 @@ namespace CA_DataUploaderLib
             LoadPlugin(fullpath);
         }
 
-        static (AssemblyLoadContext context, IEnumerable<LoopControlCommand> plugins) Load(string assemblyPath, params object[] args)
+        static (AssemblyLoadContext context, IEnumerable<LoopControlCommand> plugins) Load(string assemblyFullPath, params object[] args)
         {
-            var (context, assembly) = LoadAssembly(assemblyPath);
+            var (context, assembly) = LoadAssembly(assemblyFullPath);
             return (context, CreateInstances<LoopControlCommand>(assembly, args));
         }
 
-        static (AssemblyLoadContext context, Assembly assembly) LoadAssembly(string assemblyPath)
+        static (AssemblyLoadContext context, Assembly assembly) LoadAssembly(string assemblyFullPath)
         {
-            PluginLoadContext context = new PluginLoadContext(assemblyPath);
-            using var fs = new FileStream(assemblyPath, FileMode.Open, FileAccess.Read); // force no file lock
+            PluginLoadContext context = new PluginLoadContext(assemblyFullPath);
+            using var fs = new FileStream(assemblyFullPath, FileMode.Open, FileAccess.Read); // force no file lock
             return (context, context.LoadFromStream(fs));
         }
 
@@ -121,8 +122,8 @@ namespace CA_DataUploaderLib
         {
             private readonly AssemblyDependencyResolver _resolver;
 
-            public PluginLoadContext(string pluginPath) : base (true) => 
-                _resolver = new AssemblyDependencyResolver(pluginPath);
+            public PluginLoadContext(string pluginFullPath) : base (true) => 
+                _resolver = new AssemblyDependencyResolver(pluginFullPath);
 
             protected override Assembly Load(AssemblyName assemblyName)
             {
@@ -180,6 +181,7 @@ namespace CA_DataUploaderLib
         {
             private readonly Func<(string pluginName, string targetFolder), Task> pluginDownloader;
             private readonly PluginsLoader loader;
+            private readonly string targetFolder;
 
             public override string Name => "updateplugins";
 
@@ -187,11 +189,12 @@ namespace CA_DataUploaderLib
 
             public override bool IsHiddenCommand => true;
 
-            public UpdatePluginsCommand(CommandHandler cmd, Func<(string pluginName, string targetFolder), Task> pluginDownloader, PluginsLoader loader)
+            public UpdatePluginsCommand(CommandHandler cmd, Func<(string pluginName, string targetFolder), Task> pluginDownloader, PluginsLoader loader, string targetFolder)
             {
                 Initialize(new PluginsCommandHandler(cmd), new PluginsLogger("PluginsUpdater"));
                 this.pluginDownloader = pluginDownloader;
                 this.loader = loader;
+                this.targetFolder = targetFolder;
             }
 
             protected override Task Command(List<string> args) => args.Count > 1 ? UpdatePlugin(args[1]) : UpdateAllPlugins();
@@ -201,7 +204,7 @@ namespace CA_DataUploaderLib
                 foreach (var pluginName in loader.GetRunningPluginsNames())
                 {
                     CALog.LogInfoAndConsoleLn(LogID.A, $"downloading plugin: {pluginName}");
-                    await pluginDownloader((pluginName, "."));
+                    await pluginDownloader((pluginName, targetFolder));
                 }
 
                 CALog.LogInfoAndConsoleLn(LogID.A, $"unloading running plugins");
@@ -214,12 +217,12 @@ namespace CA_DataUploaderLib
             private async Task UpdatePlugin(string pluginName)
             {
                 CALog.LogInfoAndConsoleLn(LogID.A, $"downloading plugin: {pluginName}");
-                await pluginDownloader((pluginName, "."));
-                var assemblyPath = Path.GetFullPath(pluginName + ".dll");
+                await pluginDownloader((pluginName, targetFolder));
+                var assemblyFullPath = Path.Combine(targetFolder, Path.GetFullPath(pluginName + ".dll"));
                 CALog.LogInfoAndConsoleLn(LogID.A, $"unloading plugin: {pluginName}");
-                loader.UnloadPlugin(assemblyPath);
+                loader.UnloadPlugin(assemblyFullPath);
                 CALog.LogInfoAndConsoleLn(LogID.A, $"loading plugin: {pluginName}");
-                loader.LoadPlugin(assemblyPath);
+                loader.LoadPlugin(assemblyFullPath);
                 CALog.LogInfoAndConsoleLn(LogID.A, $"plugins updated");
             }
         }