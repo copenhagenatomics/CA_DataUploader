copenhagenatomics/CA_DataUploader

commit 0d5e4c56a47cb27735d44a7085d069f3eba82533

updates to make the config to plots values more predictable + allow system to start in more cases (#132)
* system now reports the same amount of values for missing boards
* turned some local warnings into alerts that will also show in the event log
* system still starts when there are missing heaters
* system now reports device detection events, which adds device detection and calibration updates info to the event log
* system temperature names now include the loop name as a prefix
* the subsystems are now explicitely run via CommandHandler.RunSubSystems
   Reason: fixes bug where alerts related to missing boards were not being sent during initialization. This is because the alerts would trigger before the ServerUploader was subscribed to CommandHandler.EventFired

CA_DataUploaderLib/CommandHandler.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CA_DataUploaderLib
 {
@@ -22,7 +23,7 @@ namespace CA_DataUploaderLib
         private readonly List<string> AcceptedCommands = new List<string>();
         private readonly List<ISubsystemWithVectorData> _subsystems = new List<ISubsystemWithVectorData>();
         private int AcceptedCommandsIndex = -1;
-        private Lazy<VectorFilterAndMath> _fullsystemFilterAndMath;
+        private readonly Lazy<VectorFilterAndMath> _fullsystemFilterAndMath;
 
         public event EventHandler<NewVectorReceivedArgs> NewVectorReceived;
         public event EventHandler<EventFiredArgs> EventFired;
@@ -71,6 +72,25 @@ namespace CA_DataUploaderLib
             OnNewVectorReceived(samples.WithVectorTime(vectorTime));
             return (samples, vectorTime);
         }
+
+        public Task RunSubsystems()
+        {
+            Execute("help");
+            SendDeviceDetectionEvent();
+            return Task.WhenAll(_subsystems.Select(s => s.Run(CancellationToken.None)));
+        }
+
+        private void SendDeviceDetectionEvent()
+        {
+            var sb = new StringBuilder();
+            foreach (var msg in _mapper.CalibrationUpdateMessages)
+                sb.AppendLine(msg);
+            sb.AppendLine("Detected devices:");
+            foreach (var board in _mapper.McuBoards)
+                sb.AppendLine(board.ToShortDescription());
+            FireCustomEvent(sb.ToString(), DateTime.UtcNow, (byte)EventType.SystemChangeNotification);
+        }
+
         private VectorFilterAndMath GetFullSystemFilterAndMath()
         { 
             var items = new List<VectorDescriptionItem>(_subsystems.Count * 10);
@@ -87,7 +107,7 @@ namespace CA_DataUploaderLib
      
         public bool AssertArgs(List<string> args, int minimumLen)
         {
-            if (args.Count() < minimumLen)
+            if (args.Count < minimumLen)
             {
                 CALog.LogInfoAndConsoleLn(LogID.A, "Too few arguments for this command");
                 Execute("help", false);
@@ -101,7 +121,12 @@ namespace CA_DataUploaderLib
             NewVectorReceived?.Invoke(this, new NewVectorReceivedArgs(vector.ToDictionary(v => v.Name, v => v.Value)));
 
         public void FireAlert(string msg) => FireAlert(msg, DateTime.UtcNow);
-        public void FireAlert(string msg, DateTime timespan) => FireCustomEvent(msg, timespan, (byte)EventType.Alert);
+        public void FireAlert(string msg, DateTime timespan)
+        {
+            CALog.LogErrorAndConsoleLn(LogID.A, msg);
+            FireCustomEvent(msg, timespan, (byte)EventType.Alert);
+        }
+
         /// <summary>registers a custom event (low frequency, such like user commands and alerts that have a max firing rate)</summary>
         /// <remarks>preferably use values above 100 for eventType to avoid future collisions with built in event types</remarks>
         public void FireCustomEvent(string msg, DateTime timespan, byte eventType) => EventFired?.Invoke(this, new EventFiredArgs(msg, eventType, timespan));
@@ -253,8 +278,8 @@ namespace CA_DataUploaderLib
             return inputCommand.ToString();
         }
 
-        public static int GetCmdParam(List<string> cmd, int index, int defaultValue) => 
-            cmd.Count() > index && int.TryParse(cmd[index], out int value) ? value : defaultValue;
+        public static int GetCmdParam(List<string> cmd, int index, int defaultValue) =>
+            cmd.Count > index && int.TryParse(cmd[index], out int value) ? value : defaultValue;
 
         private void RestoreNextAcceptedCommand()
         {

CA_DataUploaderLib/HeatingController.cs
@@ -4,6 +4,7 @@ using CA_DataUploaderLib.IOconf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CA_DataUploaderLib
@@ -13,14 +14,16 @@ namespace CA_DataUploaderLib
         public string Title => "Heaters";
         private bool _disposed = false;
         private readonly List<HeaterElement> _heaters = new List<HeaterElement>();
-        private PluginsCommandHandler _cmd;
-        private SwitchBoardController _switchboardController;
+        private readonly PluginsCommandHandler _cmd;
+        private readonly SwitchBoardController _switchboardController;
         private readonly OvenCommand _ovenCmd;
         private readonly HeaterCommand _heaterCmd;
+        private readonly CommandHandler _cmdUnwrapped;
 
         public HeatingController(CommandHandler cmd)
         {
             _cmd = new PluginsCommandHandler(cmd);
+            _cmdUnwrapped = cmd;
 
             var heatersConfigs = IOconfFile.GetHeater().ToList();
             if (!heatersConfigs.Any())
@@ -31,12 +34,6 @@ namespace CA_DataUploaderLib
                 _heaters.Add(new HeaterElement(
                     heater, 
                     ovens.SingleOrDefault(x => x.HeatingElement.Name == heater.Name)));
-
-            var unreachableBoards = heatersConfigs.Where(h => h.Map.Board == null).GroupBy(h => h.Map).ToList();
-            foreach (var board in unreachableBoards)
-                CALog.LogErrorAndConsoleLn(LogID.A, $"Missing board {board.Key} for heaters {string.Join(",", board.Select(h => h.Name))}");
-            if (unreachableBoards.Count > 0)
-                throw new NotSupportedException("Running with missing heaters is not currently supported");
 
             cmd.AddCommand("emergencyshutdown", EmergencyShutdown);    
             cmd.AddSubsystem(this);
@@ -51,6 +48,14 @@ namespace CA_DataUploaderLib
         {
             _cmd.Execute("oven off", false);
             return true;
+        }
+
+        public Task Run(CancellationToken token)
+        {
+            foreach (var heater in _heaters)
+                heater.ReportDetectedConfigAlerts(_cmdUnwrapped);
+
+            return _switchboardController.Run(token);
         }
 
         public IEnumerable<SensorSample> GetInputValues() => Enumerable.Empty<SensorSample>();

CA_DataUploaderLib/SwitchBoardController.cs
@@ -17,22 +17,34 @@ namespace CA_DataUploaderLib
         /// <remarks>some boards might be closed, specially if the system is stopping due to losing connection to one of the boards</remarks>
         private readonly BaseSensorBox _reader;
         private static readonly object ControllerInitializationLock = new object();
-        private PluginsCommandHandler _cmd;
+        private readonly PluginsCommandHandler _cmd;
+        private readonly List<IOconfOut230Vac> _ports;
         private static SwitchBoardController _instance;
         private readonly TaskCompletionSource _boardControlLoopsStopped = new TaskCompletionSource();
         private readonly CancellationTokenSource _boardLoopsStopTokenSource = new CancellationTokenSource();
+        private Task _runningTask;
 
         private SwitchBoardController(CommandHandler cmd) 
         {
             _cmd = new PluginsCommandHandler(cmd);
-            var ports = IOconfFile.GetEntries<IOconfOut230Vac>().Where(p => p.IsSwitchboardControllerOutput && p.Map.Board != null).ToList();
-            var boardsTemperatures = ports.GroupBy(p => p.BoxName).Select(b => b.Select(p => p.GetBoardTemperatureInputConf()).FirstOrDefault());
+            _ports = IOconfFile.GetEntries<IOconfOut230Vac>().Where(p => p.IsSwitchboardControllerOutput).ToList();
+            var boardsTemperatures = _ports.GroupBy(p => p.BoxName).Select(b => b.Select(p => p.GetBoardTemperatureInputConf()).FirstOrDefault());
             var sensorPortsInputs = IOconfFile.GetEntries<IOconfSwitchboardSensor>().SelectMany(i => i.GetExpandedConf());
-            var inputs = ports.SelectMany(p => p.GetExpandedInputConf()).Concat(boardsTemperatures).Concat(sensorPortsInputs);
+            var inputs = _ports.SelectMany(p => p.GetExpandedInputConf()).Concat(boardsTemperatures).Concat(sensorPortsInputs);
             _reader = new BaseSensorBox(cmd, "switchboards", string.Empty, "show switchboards inputs", inputs);
             _reader.Stopping += WaitForLoopStopped;
             cmd.AddCommand("escape", Stop);
-            Task.Run(() => RunBoardControlLoops(ports));
+        }
+
+        public Task Run(CancellationToken token)
+        {
+            lock (this)
+            { //important: the lock is only to start the task i.e. it is intended that there are no awaits while in the lock section.
+                if (_runningTask != null) return _runningTask;
+                //the Task.Run around the _reader.Run is because _reader.Run can raise the _reader.Stopping event in its sync code path.
+                //this prevents continuing the sync code path of this method (and the caller) while Stopping finishes being raised, which in turn would deadlock as it waits on RunBoardControlLoops to finish running.
+                return _runningTask = Task.WhenAll(Task.Run(() => _reader.Run(token)), RunBoardControlLoops(_ports, token));
+            }
         }
 
         public void Dispose() 
@@ -61,7 +73,7 @@ namespace CA_DataUploaderLib
         {
             var lastActions = new SwitchboardAction[ports.Max(p => p.PortNumber)];
             var boardStateName = board.BoxName + "_state";
-            // we use the next 2 booleans to avoid spamming logs/display with an ongoing problem, so we only notify at the beginning and when we resume normal operatio.
+            // we use the next 2 booleans to avoid spamming logs/display with an ongoing problem, so we only notify at the beginning and when we resume normal operation.
             // we might still get lots of entries for problems that alternate between normal and failed states, but for now is a good data point to know if that case is happening.
             var waitingBoardReconnect = false;
             var tryingToRecoverAfterTimeoutWatch = new Stopwatch();
@@ -90,7 +102,9 @@ namespace CA_DataUploaderLib
                         CALog.LogInfoAndConsoleLn(LogID.A, $"timed out writing to switchboard, reducing action frequency until reconnect - {board.ToShortDescription()}");
                         tryingToRecoverAfterTimeoutWatch.Restart();
                     }
-                    await Task.Delay(500); // forcing reduced acting frequency
+                    // forcing reduced acting frequency )
+                    try { await Task.Delay(500, token); } 
+                    catch (TaskCanceledException) { }
                 }
                 catch (TaskCanceledException ex)
                 {
@@ -102,7 +116,7 @@ namespace CA_DataUploaderLib
                     CALog.LogErrorAndConsoleLn(LogID.A, ex.ToString());
                 }
             }
-            
+
             AllOff(board, ports);
         }
 
@@ -122,14 +136,16 @@ namespace CA_DataUploaderLib
             return connected;
         }
 
-        private async Task RunBoardControlLoops(List<IOconfOut230Vac> ports)
+        private async Task RunBoardControlLoops(List<IOconfOut230Vac> ports, CancellationToken token)
         {
             DateTime start = DateTime.Now;
             try
             {
+                using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(token, _boardLoopsStopTokenSource.Token);
                 var boardLoops = ports
+                    .Where(p => p.Map.Board != null) //we ignore the missing boards for now as we don't have auto reconnect logic yet for boards not detected during system start. Note the reader in the ctor already reports the missing board.
                     .GroupBy(v => v.Map.Board)
-                    .Select(g => BoardLoop(g.Key, g.ToList(), _boardLoopsStopTokenSource.Token))
+                    .Select(g => BoardLoop(g.Key, g.ToList(), linkedCts.Token))
                     .ToList();
                 await Task.WhenAll(boardLoops);                
                 CALog.LogInfoAndConsoleLn(LogID.A, "Exiting SwitchBoardController.RunBoardControlLoops() " + DateTime.Now.Subtract(start).Humanize(5));
@@ -144,7 +160,7 @@ namespace CA_DataUploaderLib
             }
         }
         
-        private async Task DoPortActions(NewVectorReceivedArgs vector, MCUBoard board, IOconfOut230Vac port, SwitchboardAction[] lastActions, CancellationToken token)
+        private static async Task DoPortActions(NewVectorReceivedArgs vector, MCUBoard board, IOconfOut230Vac port, SwitchboardAction[] lastActions, CancellationToken token)
         {
             if (token.IsCancellationRequested)
                 return;
@@ -175,7 +191,7 @@ namespace CA_DataUploaderLib
             }
         }
 
-        private void AllOff(MCUBoard board, List<IOconfOut230Vac> ports)
+        private static void AllOff(MCUBoard board, List<IOconfOut230Vac> ports)
         {
             try
             {

CA_DataUploader/Program.cs
@@ -26,7 +26,7 @@ namespace CA_DataUploader
                         return; // SerialNumberMapper already lists devices, no need for further output.
 
                     // close all ports which are not Hub10
-                    serial.McuBoards.Where(x => !x.productType.Contains("Temperature")).ToList().ForEach(x => x.SafeClose(System.Threading.CancellationToken.None).Wait());
+                    serial.McuBoards.Where(x => !x.productType.Contains("Temperature") &&!x.productType.Contains("Hub10STM")).ToList().ForEach(x => x.SafeClose(System.Threading.CancellationToken.None).Wait());
 
                     var email = IOconfSetup.UpdateIOconf(serial);
 
@@ -34,6 +34,7 @@ namespace CA_DataUploader
                     using var usb = new ThermocoupleBox(cmd);
                     using var cloud = new ServerUploader(cmd.GetFullSystemVectorDescription(), cmd);
                     CALog.LogInfoAndConsoleLn(LogID.A, "Now connected to server...");
+                    _ = Task.Run(() => cmd.RunSubsystems());
 
                     int i = 0;
                     var uploadThrottle = new TimeThrottle(100);

CA_DataUploaderLib/Alerts.cs
@@ -106,16 +106,14 @@ namespace CA_DataUploaderLib
 
         private void TriggerAlert(IOconfAlert a, DateTime timestamp, string message)
         {
-            logger.LogError(timestamp.ToString("yyyy.MM.dd HH:mm:ss") + message);
+            _cmd.FireAlert(message, timestamp);
             if (a.Command != default)
             {
                 foreach (var commands in a.Command.Split('|'))
                     ExecuteCommand(commands);
             }
-
-            _cmd.FireAlert(message, timestamp);            
         }
 
-        private List<T> EnsureInitialized<T>(ref List<T> list) => list = list ?? new List<T>();
+        private static List<T> EnsureInitialized<T>(ref List<T> list) => list = list ?? new List<T>();
     }
 }

commit ad094bdf159efaeb24916630829449fc758e56ba

fixed switchboard comms regression introduced by #132
The problem was SwitchBoardController.Run was explicitely running the _reader loop while it already automatically ran as it is registered as a subsystem

The controller also now registers itself as a subsystem removing unnecesary locking and also making the subtle requirement on _reader vs. actuation loops start explicit.

CA_DataUploaderLib/CA_DataUploaderLib.csproj
@@ -43,9 +43,9 @@
       <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
       <PrivateAssets>all</PrivateAssets>
     </PackageReference>
-    <PackageReference Include="System.IO.Ports" Version="6.0.0-rc.1.21451.13" />
+    <PackageReference Include="System.IO.Ports" Version="6.0.0-rc.2.21480.5" />
     <PackageReference Include="CA.LoopControlPluginBase" Version="0.0.2" />
-    <PackageReference Include="System.IO.Pipelines" Version="6.0.0-rc.1.21451.13" />
+    <PackageReference Include="System.IO.Pipelines" Version="6.0.0-rc.2.21480.5" />
   </ItemGroup>
 
   <ItemGroup>

CA_DataUploaderLib/CommandHandler.cs
@@ -97,7 +97,8 @@ namespace CA_DataUploaderLib
             foreach (var subsystem in _subsystems)
             {
                 var subsystemItems = subsystem.GetVectorDescriptionItems();
-                CALog.LogInfoAndConsoleLn(LogID.A, $"{subsystemItems.Count,2} datapoints from {subsystem.Title}");
+                if (subsystemItems.Count > 0)
+                    CALog.LogInfoAndConsoleLn(LogID.A, $"{subsystemItems.Count,2} datapoints from {subsystem.Title}");
                 items.AddRange(subsystemItems);
             }
 

CA_DataUploaderLib/HeatingController.cs
@@ -52,10 +52,12 @@ namespace CA_DataUploaderLib
 
         public Task Run(CancellationToken token)
         {
+            //we only report detected config alerts after all subsystems are initialized (and alerts can be delivered).
+            //the actual read and actuation is done by the SwitchBoardController subsystem.
             foreach (var heater in _heaters)
                 heater.ReportDetectedConfigAlerts(_cmdUnwrapped);
 
-            return _switchboardController.Run(token);
+            return Task.CompletedTask; 
         }
 
         public IEnumerable<SensorSample> GetInputValues() => Enumerable.Empty<SensorSample>();

CA_DataUploaderLib/SwitchBoardController.cs
@@ -11,10 +11,8 @@ using Humanizer;
 
 namespace CA_DataUploaderLib
 {
-    public sealed class SwitchBoardController : IDisposable
+    public sealed class SwitchBoardController : IDisposable, ISubsystemWithVectorData
     {
-        /// <summary>runs when the subsystem is about to stop running, but before all boards are closed</summary>
-        /// <remarks>some boards might be closed, specially if the system is stopping due to losing connection to one of the boards</remarks>
         private readonly BaseSensorBox _reader;
         private static readonly object ControllerInitializationLock = new object();
         private readonly PluginsCommandHandler _cmd;
@@ -22,7 +20,8 @@ namespace CA_DataUploaderLib
         private static SwitchBoardController _instance;
         private readonly TaskCompletionSource _boardControlLoopsStopped = new TaskCompletionSource();
         private readonly CancellationTokenSource _boardLoopsStopTokenSource = new CancellationTokenSource();
-        private Task _runningTask;
+
+        public string Title => "SwitchboardActuation"; //not really displayed as this subsystem does not have data points
 
         private SwitchBoardController(CommandHandler cmd) 
         {
@@ -31,22 +30,16 @@ namespace CA_DataUploaderLib
             var boardsTemperatures = _ports.GroupBy(p => p.BoxName).Select(b => b.Select(p => p.GetBoardTemperatureInputConf()).FirstOrDefault());
             var sensorPortsInputs = IOconfFile.GetEntries<IOconfSwitchboardSensor>().SelectMany(i => i.GetExpandedConf());
             var inputs = _ports.SelectMany(p => p.GetExpandedInputConf()).Concat(boardsTemperatures).Concat(sensorPortsInputs);
+            //we register the subsystem before the reader auto registers, ensuring our Run is called first.
+            //reason: if there are no boards connected, _reader.Stopping can be raised synchronously
+            //        preventing our sub system from running and causing a deadlock as it WaitForLoopStopped.
+            cmd.AddSubsystem(this); 
             _reader = new BaseSensorBox(cmd, "switchboards", string.Empty, "show switchboards inputs", inputs);
             _reader.Stopping += WaitForLoopStopped;
             cmd.AddCommand("escape", Stop);
         }
 
-        public Task Run(CancellationToken token)
-        {
-            lock (this)
-            { //important: the lock is only to start the task i.e. it is intended that there are no awaits while in the lock section.
-                if (_runningTask != null) return _runningTask;
-                //the Task.Run around the _reader.Run is because _reader.Run can raise the _reader.Stopping event in its sync code path.
-                //this prevents continuing the sync code path of this method (and the caller) while Stopping finishes being raised, which in turn would deadlock as it waits on RunBoardControlLoops to finish running.
-                return _runningTask = Task.WhenAll(Task.Run(() => _reader.Run(token)), RunBoardControlLoops(_ports, token));
-            }
-        }
-
+        public Task Run(CancellationToken token) => RunBoardControlLoops(_ports, token);
         public void Dispose() 
         { 
             _boardLoopsStopTokenSource.Cancel();
@@ -212,5 +205,9 @@ namespace CA_DataUploaderLib
             _boardControlLoopsStopped.Task.Wait();
             CALog.LogData(LogID.A, "finished waiting for switchboards loops");
         }
+
+        public List<VectorDescriptionItem> GetVectorDescriptionItems() => new List<VectorDescriptionItem>();
+        public IEnumerable<SensorSample> GetInputValues() => Enumerable.Empty<SensorSample>();
+        public IEnumerable<SensorSample> GetDecisionOutputs(NewVectorReceivedArgs inputVectorReceivedArgs) => Enumerable.Empty<SensorSample>();
     }
 }