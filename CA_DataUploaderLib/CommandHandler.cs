d4908d7a09e97fbf58a1a8da1bbdd0ad867744b9
freddyrios <freddy.rios@copenhagenatomics.com>
2022-03-25 17:18:48 +0100

    uploads additional mcu board detected information

--- CA_DataUploaderLib/CommandHandler.cs
+++ CA_DataUploaderLib/CommandHandler.cs
@@ -89,14 +89,24 @@ namespace CA_DataUploaderLib
 
         private void SendDeviceDetectionEvent()
         {
-            var sb = new StringBuilder();
-            foreach (var board in _mapper.McuBoards)
-                if (board.Calibration != board.UpdatedCalibration)
-                    sb.AppendLine($"replaced board calibration '{board.Calibration}' with '{board.UpdatedCalibration}");
-            sb.AppendLine("Detected devices:");
-            foreach (var board in _mapper.McuBoards)
-                sb.AppendLine(board.ToShortDescription());
-            FireCustomEvent(sb.ToString(), DateTime.UtcNow, (byte)EventType.SystemChangeNotification);
+            var data = new SystemChangeNotificationData() { Boards = _mapper.McuBoards.Select(ToBoardInfo).ToList() }.ToJson();
+            FireCustomEvent(data, DateTime.UtcNow, (byte)EventType.SystemChangeNotification);
+
+            static SystemChangeNotificationData.BoardInfo ToBoardInfo(MCUBoard board) => new()
+            {
+                SerialNumber = board.serialNumber,
+                ProductType = board.productType,
+                ProductSubType = board.subProductType,
+                McuFamily = board.mcuFamily,
+                SoftwareVersion = board.softwareVersion,
+                CompileDate = board.softwareCompileDate,
+                //TODO: GitSha = board.GitSha,
+                PcbVersion = board.pcbVersion,
+                Calibration = board.Calibration,
+                MappedBoardName = board.BoxName,
+                Port = board.PortName,
+                UpdatedCalibration = board.UpdatedCalibration
+            };
         }
 
         private ExtendedVectorDescription GetFullSystemFilterAndMath()
---
+++ UnitTests/SystemChangeNotificationDataTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CA_DataUploaderLib;
+using System.Text.Json;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SystemChangeNotificationDataTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var data = new SystemChangeNotificationData()
+            {
+                Boards = new() { 
+                    new () 
+                    {
+                        SerialNumber = "2B001E3335510137353430",
+                        ProductType = "AC Board",
+                        ProductSubType = "0",
+                        McuFamily = "STM32F401xB/C Rev 1",
+                        SoftwareVersion = "5e28d00",
+                        CompileDate = "2022-02-14",
+                        GitSha = "5e28d00ea474939642116b8086793900c168c7e4",
+                        PcbVersion = "5.8",
+                        Calibration = "abc",
+                        UpdatedCalibration = "ccc",
+                        MappedBoardName = "my board",
+                        Port = "usb1"
+                    },
+                    new ()
+                    {
+                        ProductType = "Temperature Board",
+                        CompileDate = "2022-01-15",
+                        PcbVersion = "4.1",
+                        MappedBoardName = "second board",
+                        Port = "usb2"
+                    }
+                }
+            };
+            var bytes = data.ToBoardsSerialInfoJsonUtf8Bytes(new DateTime(2022, 1, 1, 1, 1, 1, DateTimeKind.Utc));
+            var val = Encoding.UTF8.GetString(bytes);
+            //now simulate the receiver deserializing
+            var serials = JsonSerializer.Deserialize<McuserialNumber[]>(bytes, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+            CollectionAssert.AreEqual(new[] { "2B001E3335510137353430", "usb2" }, serials.Select(b => b.SerialNumber).ToList());
+            CollectionAssert.AreEqual(new[] { "AC Board", "Temperature Board" }, serials.Select(b => b.ProductType).ToList());
+            CollectionAssert.AreEqual(new[] { "2022-02-14", "2022-01-15" }, serials.Select(b => b.CompileDate).ToList());
+        }
+
+        //this is just used to reproduce similar parsing to the receiver of the data
+        public class McuserialNumber
+        {
+            public int? LoopNameId { get; set; }
+            public int? PlotNameId { get; set; }
+            public string SerialNumber { get; set; }
+            public string ProductType { get; set; }
+            public string ProductSubType { get; set; }
+            public string McuFamily { get; set; }
+            public string SoftwareVersion { get; set; }
+            public string CompileDate { get; set; }
+            public string GitSha { get; set; }
+            public string PcbVersion { get; set; }
+            public string Calibration { get; set; }
+            public DateTime TimeStamp { get; set; }
+        }
+    }
+}
