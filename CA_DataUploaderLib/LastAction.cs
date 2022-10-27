46f87ff8e13ad1921b4670ca8dc206de43a9b231
freddyrios <70888589+freddyrios@users.noreply.github.com>
2022-10-27 14:42:55 +0200

    simplified building subsystems actuations
    specifically added BaseSensorBox.RegisterWriteActions which is a simpler API that sends commands to the box for a given IOconfOutput.
    
    See the updated SwitchBoardController for an updated example of this in practice. Do note however that that example does not show the decision logic, as that is found in the related HeatingController.

---
+++ CA_DataUploaderLib/LastAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CA_DataUploaderLib
+{
+    public class LastAction
+    {
+        private readonly int repeatMilliseconds;
+
+        public LastAction(double target, int repeatMilliseconds)
+        {
+            Target = target;
+            this.repeatMilliseconds = repeatMilliseconds;
+        }
+
+        public double Target { get; private set; } = 0;
+        public DateTime TimeToRepeat { get; private set; }
+        public Stopwatch TimeRunning { get; } = Stopwatch.StartNew();
+        public bool ChangedOrExpired(double newtarget, DateTime currentVectorTime) =>
+            Target != newtarget || TimeRunning.ElapsedMilliseconds >= repeatMilliseconds || TimeToRepeat >= currentVectorTime;
+        public void ExecutedNewAction(double target, DateTime currentVectorTime)
+        {
+            Target = target;
+            TimeToRepeat = currentVectorTime.AddMilliseconds(repeatMilliseconds);
+            TimeRunning.Restart();
+        }
+
+        public void TimedOutWaittingForDecision(double target)
+        {
+            //DateTime.MavValue forces execution on the next vector / also note we don't restart time running for the same reason.
+            Target = target;
+            TimeToRepeat = DateTime.MaxValue;
+        }
+    }
+}
