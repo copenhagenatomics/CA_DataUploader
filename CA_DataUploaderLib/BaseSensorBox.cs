copenhagenatomics/CA_DataUploader

commit 956a2f6852cb233c926751853b8f976bd9a9bb7f

simplified building subsystems actuations (#199)
specifically added BaseSensorBox.RegisterWriteActions which is a simpler API that sends commands to the box for a given IOconfOutput.

See the updated SwitchBoardController for an updated example of this in practice. Do note however that that example does not show the decision logic, as that is found in the related HeatingController.

CA_DataUploaderLib/BaseSensorBox.cs
@@ -134,6 +134,46 @@ namespace CA_DataUploaderLib
             else actions.Add((writeAction, exitAction));
         }
 
+        protected void RegisterBoardWriteActions(
+            MCUBoard board, IOconfOutput port, double defaultTarget, string targetFieldName, 
+            Func<int, double, string> getCommand, int repeatMilliseconds = 2000)
+        {
+            var lastAction = new LastAction(defaultTarget, repeatMilliseconds);
+            var fieldIndex = -1;
+            _cmd.FullVectorIndexesCreated += InitializeAction;
+            AddBuildInWriteAction(board, WriteAction, ExitAction);
+
+            void InitializeAction(object? sender, IReadOnlyDictionary<string, int> indexes) =>
+                fieldIndex = indexes.TryGetValue(targetFieldName, out var index) 
+                    ? index 
+                    : throw new InvalidOperationException($"missing target field: {targetFieldName}");
+            Task ExitAction(MCUBoard board, CancellationToken token) => Off(board, port, token);
+            Task WriteAction(DataVector? vector, MCUBoard board, CancellationToken token) => DoPortActions(vector, board, fieldIndex, lastAction, token);
+
+            async Task DoPortActions(DataVector? vector, MCUBoard board, int fieldIndex, LastAction lastAction, CancellationToken token)
+            {
+                if (vector == null)
+                {
+                    await board.SafeWriteLine(getCommand(port.PortNumber, defaultTarget), token);
+                    lastAction.TimedOutWaittingForDecision(defaultTarget);
+                    return;
+                }
+
+                var target = vector[fieldIndex];
+                if (lastAction.ChangedOrExpired(target, vector.Timestamp))
+                    return;
+
+                await board.SafeWriteLine(getCommand(port.PortNumber, target), token);
+                lastAction.ExecutedNewAction(target, vector.Timestamp);
+            }
+
+            async Task Off(MCUBoard board, IOconfOutput port, CancellationToken token)
+            {
+                await board.SafeWriteLine(getCommand(port.PortNumber, defaultTarget), token);
+                CALog.LogInfoAndConsoleLn(LogID.A, $"port has been set to default position ({defaultTarget:F2}): {port.Name}");
+            }
+        }
+
         protected bool ShowQueue(List<string> args)
         {
             var subsystem = args[0].ToLower();

CA_DataUploaderLib/IOconf/IOconfOut230Vac.cs
@@ -15,7 +15,6 @@ namespace CA_DataUploaderLib.IOconf
 
         public string CurrentSensorName { get; }
         public string BoardStateSensorName { get; } 
-        public bool HasOnSafeState { get; } = false;
         public bool IsSwitchboardControllerOutput { get; }
         public IEnumerable<IOconfInput> GetExpandedInputConf()
         { // note "_onoff" is not included as its not an input but the current expected on/off state as seen by the control loop.

CA_DataUploaderLib/LastAction.cs
@@ -1,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CA_DataUploaderLib
+{
+    public class LastAction
+    {
+        private readonly int repeatMilliseconds;
+
+        public LastAction(double target, int repeatMilliseconds)
+        {
+            Target = target;
+            this.repeatMilliseconds = repeatMilliseconds;
+        }
+
+        public double Target { get; private set; } = 0;
+        public DateTime TimeToRepeat { get; private set; }
+        public Stopwatch TimeRunning { get; } = Stopwatch.StartNew();
+        public bool ChangedOrExpired(double newtarget, DateTime currentVectorTime) =>
+            Target != newtarget || TimeRunning.ElapsedMilliseconds >= repeatMilliseconds || TimeToRepeat >= currentVectorTime;
+        public void ExecutedNewAction(double target, DateTime currentVectorTime)
+        {
+            Target = target;
+            TimeToRepeat = currentVectorTime.AddMilliseconds(repeatMilliseconds);
+            TimeRunning.Restart();
+        }
+
+        public void TimedOutWaittingForDecision(double target)
+        {
+            //DateTime.MavValue forces execution on the next vector / also note we don't restart time running for the same reason.
+            Target = target;
+            TimeToRepeat = DateTime.MaxValue;
+        }
+    }
+}

CA_DataUploaderLib/SwitchBoardController.cs
@@ -1,29 +1,26 @@
 ﻿#nullable enable
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using CA_DataUploaderLib.IOconf;
 
 namespace CA_DataUploaderLib
 {
-    public sealed class SwitchBoardController
+    public sealed class SwitchBoardController : BaseSensorBox
     {
         private static readonly object ControllerInitializationLock = new();
         private static SwitchBoardController? _instance;
+        private static readonly List<IOconfOut230Vac> ports = IOconfFile.GetEntries<IOconfOut230Vac>().ToList();
 
-        private SwitchBoardController(CommandHandler cmd) 
+        private SwitchBoardController(CommandHandler cmd) : base(cmd, "switchboards",
+            ports.SelectMany(p => p.GetExpandedInputConf())
+            .Concat(ports.GroupBy(p => p.BoxName).Select(g => g.First().GetBoardTemperatureInputConf()))
+            .Concat(IOconfFile.GetEntries<IOconfSwitchboardSensor>().SelectMany(i => i.GetExpandedConf())))
         {
-            var ports = IOconfFile.GetEntries<IOconfOut230Vac>().ToList();
-            var boardsTemperatures = ports.GroupBy(p => p.BoxName).Select(g => g.First().GetBoardTemperatureInputConf());
-            var sensorPortsInputs = IOconfFile.GetEntries<IOconfSwitchboardSensor>().SelectMany(i => i.GetExpandedConf());
-            var inputs = ports.SelectMany(p => p.GetExpandedInputConf()).Concat(boardsTemperatures).Concat(sensorPortsInputs);
-            var boardsLoops = new BaseSensorBox(cmd, "switchboards", inputs);
             //we ignore remote boards and boards missing during the start sequence (as we don't have auto reconnect logic yet for those). Note the BaseSensorBox already reports the missing local boards.
-            foreach (var board in ports.Where(p => p.Map.IsLocalBoard && p.Map.Board != null).GroupBy(v => v.Map.Board!))
-                RegisterBoardWriteActions(cmd, boardsLoops, board.Key, board.ToList());
+            foreach (var port in ports.Where(p => p.Map.IsLocalBoard && p.Map.Board != null))
+                RegisterBoardWriteActions(port.Map.Board!, port, 0, port.Name + "_onoff", GetCommand);
+
+            static string GetCommand(int portNumber, double target) => target == 1.0 ? $"p{portNumber} on 3" : $"p{portNumber} off";
         }
 
         public static void Initialize(CommandHandler cmd)
@@ -31,66 +28,6 @@ namespace CA_DataUploaderLib
             if (_instance != null) return;
             lock (ControllerInitializationLock)
                 _instance ??= new SwitchBoardController(cmd);
-        }
-
-        private static void RegisterBoardWriteActions(CommandHandler cmd, BaseSensorBox reader, MCUBoard board, List<IOconfOut230Vac> ports)
-        {
-            var lastActions = Enumerable.Range(0, ports.Max(p => p.PortNumber))
-                .Select(_ => (isOn: false, timeToRepeat: default(DateTime), timeRunnin: Stopwatch.StartNew()))
-                .ToArray();
-            (int fieldIndex, int number)[] portsFields = Array.Empty<(int fieldIndex, int number)>();
-            cmd.FullVectorIndexesCreated += InitializeAction;
-            reader.AddBuildInWriteAction(board, WriteAction, ExitAction);
-
-            void InitializeAction(object? sender, IReadOnlyDictionary<string, int> indexes) => 
-                portsFields = ports.Select(p => (
-                    fieldIndex: indexes.TryGetValue(p.Name + "_onoff", out var index) 
-                        ? index 
-                        : throw new InvalidOperationException($"failed to find {p.Name + "_onoff"} in the full vector"), 
-                    number: p.PortNumber)).ToArray();
-            Task ExitAction(MCUBoard board, CancellationToken token) => AllOff(board, ports, token);
-            async Task WriteAction(DataVector? vector, MCUBoard board, CancellationToken token)
-            {
-                foreach (var port in portsFields)
-                    await DoPortActions(vector, board, port, lastActions, token);
-            }
-        }
-
-        private static async Task DoPortActions(DataVector? vector, MCUBoard board, (int fieldIndex, int number) port, (bool isOn, DateTime timeToRepeat, Stopwatch timeRunning)[] lastActions, CancellationToken token)
-        {
-            if (vector == null)
-            { //too long time without receiving a vector, lets ensure the port is off.
-              //note: we are assuming we get called periodically at a reasonable frequency to reassert the off, but for now we are not guarding against too frequent executions
-                await board.SafeWriteLine($"p{port.number} off", token);
-                lastActions[port.number - 1].isOn = false;
-                lastActions[port.number - 1].timeToRepeat = DateTime.MaxValue;//max forces execution on the next vector / also note we don't restart time running for the same reason.
-                return;
-            }
-
-            var isOn = vector[port.fieldIndex] == 1.0;
-            var lastAction = lastActions[port.number - 1];
-            var currentVectorTime = vector.Timestamp;
-            if (lastAction.isOn == isOn && lastAction.timeRunning.ElapsedMilliseconds < 2000 && lastAction.timeToRepeat < currentVectorTime)
-                // no action changes has been requested since the last action was executed and there has been less than 2 seconds
-                // note the 2 seconds is checked in 2 diff ways to avoid it being missed due to bit flips.
-                // Note that this does not guard on too frequent executions, but at least we are resetting both values on each execution.
-                return; 
-                
-            if (!isOn)
-                await board.SafeWriteLine($"p{port.number} off", token);
-            else
-                await board.SafeWriteLine($"p{port.number} on 3", token);
-            lastActions[port.number - 1].isOn = isOn;
-            lastActions[port.number - 1].timeToRepeat = currentVectorTime.AddMilliseconds(2000);
-            lastActions[port.number - 1].timeRunning.Restart();
-        }
-
-        private static async Task AllOff(MCUBoard board, List<IOconfOut230Vac> ports, CancellationToken token)
-        {
-            await board.SafeWriteLine("off", token); 
-            foreach (var port in ports)
-                if (port.HasOnSafeState)
-                    await board.SafeWriteLine($"p{port.PortNumber} on", token);
         }
     }
 }

commit 886090fecd7c3b3bebc2bd6f866187e3d1b00dc3

fixed regression introduced in #199

CA_DataUploaderLib/BaseSensorBox.cs
@@ -160,7 +160,7 @@ namespace CA_DataUploaderLib
                 }
 
                 var target = vector[fieldIndex];
-                if (lastAction.ChangedOrExpired(target, vector.Timestamp))
+                if (!lastAction.ChangedOrExpired(target, vector.Timestamp))
                     return;
 
                 await board.SafeWriteLine(getCommand(port.PortNumber, target), token);