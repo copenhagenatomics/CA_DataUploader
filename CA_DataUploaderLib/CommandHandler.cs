a7eff6aae92859fa37fb95401c26b46ea8f5d235
freddyrios <70888589+freddyrios@users.noreply.github.com>
2022-12-16 16:34:19 +0100

    improved command feedback
    Breaking Change: the oven command with multiple areas has been removed. This is how it now works:
    - `oven singletarget` is still supported
    - `ovenarea all singletarget` does the same thing
    - ovenarea area target
    
    It is now checked that the full decision command matches + an optional target.
    
    Note this mainly validates the command format is correct, but the decision logic can have other additional reasons to reject a command, including only the range of values accepted for the target. Check the plots to see if the system started executing the command.

--- CA_DataUploaderLib/CommandHandler.cs
+++ CA_DataUploaderLib/CommandHandler.cs
@@ -11,6 +11,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Channels;
+using CA_DataUploaderLib.Extensions;
+using System.Globalization;
 
 namespace CA_DataUploaderLib
 {
@@ -65,9 +67,27 @@ namespace CA_DataUploaderLib
             {
                 //This just avoids the commands being reported as rejected for now, but the way to go about in the long run is to add detection of the executed commands by looking at the vectors.
                 //Note that even then, the decisions are not reporting which commands they actually handled or ignore, specially as they are receiving all commands and then handle what applies to the decision.
-                var firstWhitespace = e.IndexOf(' ');
-                var firstWordInEvent = firstWhitespace != -1 ? e[..firstWhitespace] : e;
-                AddCommand(firstWordInEvent, _ => true);
+                var expectedArgs = e.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var firstWordInEvent = expectedArgs[0];
+                AddCommand(firstWordInEvent, args =>
+                {
+                    if (args.Count != expectedArgs.Length && args.Count != expectedArgs.Length + 1) 
+                        return false;
+
+                    for (int i = 1; i < expectedArgs.Length; i++)
+                    {
+                        if (!args[i].Equals(expectedArgs[i], StringComparison.OrdinalIgnoreCase))
+                            return false;
+                    }
+
+                    if (args.Count == expectedArgs.Length) 
+                        return true;
+
+                    var target = args[expectedArgs.Length];
+                    return target.TryToDouble(out _) || 
+                        target.StartsWith("0x") && uint.TryParse(target[2..], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out _) ||
+                        target.StartsWith("#") && uint.TryParse(target[1..], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out _);
+                });
             }
 
             targetList.AddRange(decisions);
