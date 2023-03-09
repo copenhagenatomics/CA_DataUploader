2177ca2f7f22cbcb7a2a3f53215376e80837386b
freddyrios <70888589+freddyrios@users.noreply.github.com>
2023-03-09 13:39:59 +0100

    ReleaseWriter/ReaderLock are no longer public methods.
    Added example usage to the xml code comment.

--- CA_DataUploaderLib/AsyncReaderWriterLock.cs
+++ CA_DataUploaderLib/AsyncReaderWriterLock.cs
@@ -9,13 +9,25 @@ namespace CA_DataUploaderLib
     /// https://stackoverflow.com/a/64757462/7293142
     /// The answered question was then improved and posted via comment here:
     /// https://github.com/copenhagenatomics/CA_DataUploader/pull/90/files#diff-24a9664c904fe9276878f37dc1438aae578a76b7ef34eabbebf6ac66eaad83e6
-    ///
-    /// This (https://gist.github.com/cajuncoding/a88f0d00847dcfc241ae80d1c7bafb1e) version adds support for simplified using(){} notation via IDisposable so that Try/Finally blocks are not needed.
+    /// Then this https://gist.github.com/cajuncoding/a88f0d00847dcfc241ae80d1c7bafb1e version adds support for simplified using(){} notation via IDisposable so that Try/Finally blocks are not needed.
     ///
     /// Additional Notes:
     /// This is an async compatible reader / writer lock; this lock allows any amount of readers to enter the lock while only a single writer can do so at a time.
     /// While the writer holds the lock, any/all readers are blocked until the writer releases the lock. It doesn't have a lot of protection, so keep its usage simple (e.g. logic flows
-    /// where try/finally can be used and no recursion or re-entry is required.
+    /// where using can be used and no recursion or re-entry is required.
+    /// 
+    /// Taking a reader lock: 
+    /// <code>
+    ///     using var _ = lock.AcquireReaderLock(cancellationToken);
+    ///     // code goes here ... the lock is taken until the end of the current method
+    /// </code>
+    /// 
+    /// Taking a writer lock: 
+    /// <code>
+    ///     using var _ = lock.AcquireWriterLock(cancellationToken);
+    ///     // code goes here ... the lock is taken until the end of the current method
+    /// </code>
+    /// 
     /// How it works:
     ///  - Two semaphores amd a Count of readers in the lock are used to provide the above guarantees.
     ///  - To guarantee no new readers or writers can enter the lock while a writer is active, a write semaphore is used
@@ -29,8 +41,8 @@ namespace CA_DataUploaderLib
     /// </summary>
     public sealed class AsyncReaderWriterLock : IDisposable
     {
-        readonly SemaphoreSlim _readSemaphore = new SemaphoreSlim(1, 1);
-        readonly SemaphoreSlim _writeSemaphore = new SemaphoreSlim(1, 1);
+        readonly SemaphoreSlim _readSemaphore = new(1, 1);
+        readonly SemaphoreSlim _writeSemaphore = new(1, 1);
         int _readerCount;
 
         public async Task<IDisposable> AcquireWriterLock(CancellationToken token = default)
@@ -47,12 +59,12 @@ namespace CA_DataUploaderLib
             }
 
             return new LockToken(ReleaseWriterLock);
-        }
 
-        public void ReleaseWriterLock()
-        {
-            _readSemaphore.Release();
-            _writeSemaphore.Release();
+            void ReleaseWriterLock()
+            {
+                _readSemaphore.Release();
+                _writeSemaphore.Release();
+            }
         }
 
         public async Task<IDisposable> AcquireReaderLock(CancellationToken token = default)
@@ -74,12 +86,12 @@ namespace CA_DataUploaderLib
 
             _writeSemaphore.Release();
             return new LockToken(ReleaseReaderLock);
-        }
 
-        public void ReleaseReaderLock()
-        {
-            if (Interlocked.Decrement(ref _readerCount) == 0)
-                _readSemaphore.Release();
+            void ReleaseReaderLock()
+            {
+                if (Interlocked.Decrement(ref _readerCount) == 0)
+                    _readSemaphore.Release();
+            }
         }
 
         public void Dispose()
