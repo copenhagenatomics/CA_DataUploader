dca0c24c2418aaed543adc7ea4dbe24e8488e048
freddyrios <70888589+freddyrios@users.noreply.github.com>
2023-03-20 15:47:32 +0100

    allows subsystems using BaseSensorBox to disable repeat commands

--- UnitTests/LastActionTests.cs
+++ UnitTests/LastActionTests.cs
@@ -13,7 +13,7 @@ namespace UnitTests
         {
             var now = DateTime.UtcNow;
             var action = new LastAction(0.1, 500);
-            Assert.IsFalse(action.ChangedOrExpired(0.1, now), "expiration is expected before executing the first action");
+            Assert.IsTrue(action.ChangedOrExpired(0.1, now), "expiration is expected before executing the first action");
             now = now.AddSeconds(1);
             action.ExecutedNewAction(0.2, now);
             Assert.IsFalse(action.ChangedOrExpired(0.2, now), "2: no expiration expected at 0s");
@@ -32,13 +32,31 @@ namespace UnitTests
         {
             var now = DateTime.UtcNow;
             var action = new LastAction(0.1, 100);
-            Assert.IsFalse(action.ChangedOrExpired(0.1, now), "expiration is expected before executing the first action");
+            Assert.IsTrue(action.ChangedOrExpired(0.1, now), "expiration is expected before executing the first action");
             action.ExecutedNewAction(0.2, now);
             Assert.IsFalse(action.ChangedOrExpired(0.2, now), "no expiration expected at 0s");
             await Task.Delay(60);
             Assert.IsFalse(action.ChangedOrExpired(0.2, now), "no expiration expected at 0.06s clock time");
             await Task.Delay(60);
             Assert.IsTrue(action.ChangedOrExpired(0.2, now), "expiration expected at 0.12s clock time");
+        }
+
+
+        [TestMethod()]
+        public void VectorOnlyNoRepeatTest()
+        {
+            var now = DateTime.UtcNow;
+            var action = new LastAction(0.1, -1);
+            Assert.IsTrue(action.ChangedOrExpired(0.1, now), "expiration is expected before executing the first action");
+            now = now.AddSeconds(1);
+            action.ExecutedNewAction(0.2, now);
+            Assert.IsFalse(action.ChangedOrExpired(0.2, now), "2: no expiration expected at 0s");
+            Assert.IsFalse(action.ChangedOrExpired(0.2, now.AddYears(10)), "2: no expiration expected at 10 years");
+            Assert.IsTrue(action.ChangedOrExpired(0.3, now.AddMilliseconds(100)), "2: target change must be detected");
+            action.ExecutedNewAction(0.3, now);
+            Assert.IsFalse(action.ChangedOrExpired(0.3, now), "3: no expiration expected at 0s");
+            Assert.IsFalse(action.ChangedOrExpired(0.3, now.AddDays(30)), "3: no expiration expected at 30 days");
+            Assert.IsTrue(action.ChangedOrExpired(0.4, now.AddMilliseconds(100)), "3: target change must be detected");
         }
     }
 }
