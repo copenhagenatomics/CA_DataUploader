ac9de321d3e05567d012abcde6e0897913fb9443
freddyrios <70888589+freddyrios@users.noreply.github.com>
2023-03-29 13:11:50 +0200

    cleaned readwrite async tests

--- UnitTests/AsyncReadWriteLockTests.cs
+++ UnitTests/AsyncReadWriteLockTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,54 +18,60 @@ namespace UnitTests
         {
             //Our race condition test works by having a mix of readers and writers that report they ran to results.
             //Readers report -1 while writers report values 0 - 10. If the reader/writer lock is working, all 10 values from the writer appear together in order in the results.
-            //We use a CountdownEvent to ensure all threads are created and ready to race before running the actions.
-            //We use SetMinThreads to ensure we have enough pool threads, as otherwise 
             var asyncReadWriteLock = new AsyncReaderWriterLock();
             var results = new ConcurrentQueue<(int index, int value)>();
-            var tasks = new ConcurrentQueue<Task>();
-            int taskCount = 100, writeBlockSize = 40, readersForEveryWriter = 9;
-            var readyEvent = new CountdownEvent(taskCount);
-            ThreadPool.SetMinThreads(taskCount, taskCount); 
-            for (var t = 0; t < taskCount; t++)
+            int writeBlockSize = 40, readersForEveryWriter = 9;
+            bool IsWriter(int index) => index % (readersForEveryWriter + 1) == 0;
+            await ForRacingThreads(0, 100, i => async () =>
             {
-                var index = t; //CAPTURE locally
-                tasks.Enqueue(Task.Run(async () =>
-                {
-                    readyEvent.Signal();
-                    readyEvent.Wait();
-                    if (index % (readersForEveryWriter + 1) == 0)
-                    {//writer: report values for the write block
-                        using var _ = await asyncReadWriteLock.AcquireWriterLock().ConfigureAwait(false);
-                        for (var r = 0; r < writeBlockSize; r++)
-                        {
-                            results.Enqueue((index, r));
-                            await Task.Yield();
-                        }
+                if (IsWriter(i))
+                {//writer: report values for the write block
+                    using var _ = await asyncReadWriteLock.AcquireWriterLock().ConfigureAwait(false);
+                    for (var r = 0; r < writeBlockSize; r++)
+                    {
+                        results.Enqueue((i, r));
+                        await Task.Yield();
                     }
-                    else
-                    {//reader: report -1
-                        using var _ = await asyncReadWriteLock.AcquireReaderLock().ConfigureAwait(false);
-                        results.Enqueue((index, -1));
-                    }
-                }));
-            }
-
-            await Task.WhenAll(tasks).ConfigureAwait(false);
+                }
+                else
+                {//reader: report -1
+                    using var _ = await asyncReadWriteLock.AcquireReaderLock().ConfigureAwait(false);
+                    results.Enqueue((i, -1));
+                }
+            }).ConfigureAwait(false);
 
             var resultsList = results.ToList();
             var failureMessage = $"unexpected value detected. Results: {string.Join(Environment.NewLine, resultsList)}";
             for (var i = 0; i < resultsList.Count; i++)
             {
-                var (index, label) = resultsList[i];
-                if (index % (readersForEveryWriter + 1) == 0)
-                {//writer: report values for the write block were reported together (without values of other readers or writers in between)
+                var index = resultsList[i].index;
+                if (IsWriter(index))
+                {//check write block was reported together (without values of other readers or writers in between)
                     for (var r = 0; r < writeBlockSize; r++)
                         Assert.AreEqual((index, r), resultsList[i + r], failureMessage);
                     i += writeBlockSize - 1; //-1 because the outer for will increment it again
                 }
-                else
+                else//check reader reported -1
                     Assert.AreEqual((index, -1), resultsList[i], failureMessage);
             }
+        }
+
+        private static Task ForRacingThreads(int start, int count, Func<int, Func<Task>> getAction) => 
+            RunRacingThreads(Enumerable.Range(start, count).Select(i => getAction(i)).ToList());
+        private static async Task RunRacingThreads(IReadOnlyCollection<Func<Task>> actions)
+        {
+            //We use a CountdownEvent to ensure all threads are created and ready to race before running the actions.
+            //We use SetMinThreads to ensure we have enough pool threads, as otherwise it takes way too long to start or even blocks
+            ThreadPool.SetMinThreads(actions.Count, actions.Count);
+            var readyEvent = new CountdownEvent(actions.Count);
+            await Task.WhenAll(actions
+                .Select(action => Task.Run(() => 
+                { 
+                    readyEvent.Signal(); //we are ready to run
+                    readyEvent.Wait();  //wait for all others to be ready to run
+                    return action(); 
+                }))
+                ).ConfigureAwait(false);
         }
     }
 }
