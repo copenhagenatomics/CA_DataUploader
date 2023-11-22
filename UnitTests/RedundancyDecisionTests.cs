746401b148732f74588c1ee3982e76ee583f2ed5
freddyrios <70888589+freddyrios@users.noreply.github.com>
2023-11-22 13:29:08 +0100

    added more redundancy strategies
    IO.conf lines: RedundantStrategy;name;[Median/Max/Min/Average]

--- UnitTests/RedundancyDecisionTests.cs
+++ UnitTests/RedundancyDecisionTests.cs
@@ -21,13 +21,36 @@ namespace UnitTests
                     new(){"a","b","c","d", "e" },
                     new(){ new() { "abox"}, new() { "bbox"}, new() { "cbox"}, new() { "dbox"}, new() { "ebox"} },
                     (0,2000),
-                    10000)),
+                    10000,
+                    Redundancy.RedundancyStrategy.Median)),
                 new Redundancy.Decision(new (
                     "red4",
                     new(){"a","b","c","d" },
                     new(){ new() { "abox"}, new() { "bbox"}, new() { "cbox"}, new() { "dbox"} },
                     (0,2000), 
-                    -0.001))},
+                    -0.001,
+                    Redundancy.RedundancyStrategy.Median)),
+                new Redundancy.Decision(new(
+                    "red5min",
+                    new() { "a", "b", "c", "d", "e" },
+                    new() { new() { "abox" }, new() { "bbox" }, new() { "cbox" }, new() { "dbox" }, new() { "ebox" } },
+                    (0, 2000),
+                    10000,
+                    Redundancy.RedundancyStrategy.Min)),
+                new Redundancy.Decision(new(
+                    "red5max",
+                    new() { "a", "b", "c", "d", "e" },
+                    new() { new() { "abox" }, new() { "bbox" }, new() { "cbox" }, new() { "dbox" }, new() { "ebox" } },
+                    (0, 2000),
+                    10000,
+                    Redundancy.RedundancyStrategy.Max)),
+                new Redundancy.Decision(new(
+                    "red5avg",
+                    new() { "a", "b", "c", "d", "e" },
+                    new() { new() { "abox" }, new() { "bbox" }, new() { "cbox" }, new() { "dbox" }, new() { "ebox" } },
+                    (0, 2000),
+                    10000,
+                    Redundancy.RedundancyStrategy.Average))},
             new()
             {
                 { "abox", (int)BaseSensorBox.ConnectionState.ReceivingValues },
@@ -94,6 +117,90 @@ namespace UnitTests
             Field("abox") = Field("bbox") = Field("cbox") = Field("dbox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
             MakeDecisions();
             Assert.AreEqual(-0.001, Field("red4"), "must use default invalid value when all sensors are stale");
+        }
+
+        [TestMethod]
+        public void UsesMinOf5()
+        {
+            MakeDecisions();
+            const string FieldName = "red5min";
+            Assert.AreEqual(10.0, this.Field(FieldName));
+            Field("c") = 21;
+            MakeDecisions();
+            Assert.AreEqual(10.0, this.Field(FieldName), "must not change min when adding a higher max");
+            Field("c") = 9;
+            MakeDecisions();
+            Assert.AreEqual(9.0, this.Field(FieldName));
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(10.0, this.Field(FieldName), "must ignore stale sensor data and use min of the remaining 4");
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.ReceivingValues;
+            Field("c") = -5;
+            MakeDecisions();
+            Assert.AreEqual(10.0, this.Field(FieldName), "must ignore stale sensor data outside the range and use min of the remaining 4");
+            Field("c") = 8;
+            MakeDecisions();
+            Assert.AreEqual(8.0, this.Field(FieldName), "must use the sensor value if it becomes valid again");
+            Field("abox") = Field("bbox") = Field("cbox") = Field("dbox") = Field("ebox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(10000, this.Field(FieldName), "must use default invalid value when all sensors are stale");
+        }
+
+
+        [TestMethod]
+        public void UsesMaxOf5()
+        {
+            MakeDecisions();
+            const string FieldName = "red5max";
+            Assert.AreEqual(20.0, this.Field(FieldName));
+            Field("c") = 2;
+            MakeDecisions();
+            Assert.AreEqual(20.0, this.Field(FieldName), "must not change max when adding a lower min");
+            Field("c") = 21;
+            MakeDecisions();
+            Assert.AreEqual(21.0, this.Field(FieldName));
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(20.0, this.Field(FieldName), "must ignore stale sensor data and use max of the remaining 4");
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.ReceivingValues;
+            Field("c") = 30000;
+            MakeDecisions();
+            Assert.AreEqual(20.0, this.Field(FieldName), "must ignore stale sensor data outside the range and use max of the remaining 4");
+            Field("c") = 22;
+            MakeDecisions();
+            Assert.AreEqual(22.0, this.Field(FieldName), "must use the sensor value if it becomes valid again");
+            Field("abox") = Field("bbox") = Field("cbox") = Field("dbox") = Field("ebox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(10000, this.Field(FieldName), "must use default invalid value when all sensors are stale");
+        }
+
+
+        [TestMethod]
+        public void UsesAverageOf5()
+        {
+            MakeDecisions();
+            const string FieldName = "red5avg";
+            Assert.AreEqual(16.0, this.Field(FieldName));
+            Field("c") = 21;
+            MakeDecisions();
+            Assert.AreEqual(16.8, this.Field(FieldName));
+            Field("c") = 13;
+            MakeDecisions();
+            Assert.AreEqual(15.2, this.Field(FieldName));
+            Field("c") = 17;
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(15.75, this.Field(FieldName), "must ignore stale sensor data and use median of the remaining 4");
+            Field("cbox") = (int)BaseSensorBox.ConnectionState.ReceivingValues;
+            Field("c") = 3000;
+            MakeDecisions();
+            Assert.AreEqual(15.75, this.Field(FieldName), "must ignore stale sensor data outside the range and use median of the remaining 4");
+            Field("c") = 17;
+            MakeDecisions();
+            Assert.AreEqual(16.0, this.Field(FieldName), "must use the sensor value if it becomes valid again");
+            Field("abox") = Field("bbox") = Field("cbox") = Field("dbox") = Field("ebox") = (int)BaseSensorBox.ConnectionState.NoDataAvailable;
+            MakeDecisions();
+            Assert.AreEqual(10000, this.Field(FieldName), "must use default invalid value when all sensors are stale");
         }
     }
 }
