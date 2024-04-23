87d1bfb501c8eae7d894d6028b91e924418d0a38
LarsWithCA <lars.with@copenhagenatomics.com>
2024-04-23 15:43:36 +0200

    IOconfFile.GetBoardStateNames: take expanded fields of all rows that implement IIOconfRowWithBoardState into account.

--- UnitTests/IOconfFileTests.cs
+++ UnitTests/IOconfFileTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CA_DataUploaderLib.Extensions;
 using CA_DataUploaderLib.IOconf;
 using System;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -24,6 +26,112 @@ namespace UnitTests
                 "Map; 1234567890; tm01",
                 "TypeJ; sameName; tm01; 1",
                 "TypeJ; sameName; tm01; 2" });
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_TemperatureField()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@$"
+Map; 4900553433511235353734; tm01
+TypeJ; temperature_tm01_01; tm01; 1
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("temperature_tm01_01").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("tm01_state", boardStateNames.First());
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_TemperatureField_Filter()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@"
+Map; 4900553433511235353734; tm01
+TypeJ; temperature_tm01_01; tm01; 1
+Filter;temperature_tm01_01; Min;600;temperature_tm01_01
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("temperature_tm01_01_filter").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("tm01_state", boardStateNames.First());
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_TemperatureField_Math()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@"
+Map; 4900553433511235353734; tm01
+TypeJ; temperature_tm01_01; tm01; 1
+Math; temperature_tm01_01_math; temperature_tm01_01 + 1
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("temperature_tm01_01_math").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("tm01_state", boardStateNames.First());
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_SwitchboardSensorExpandedInputField()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@"
+Map; 3900553433511235353736; dc01
+SwitchboardSensor; switchboardSensor_dc01; dc01
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("switchboardSensor_dc01_rms").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("dc01_state", boardStateNames.First());
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_SwitchboardSensorExpandedInputField_Filter()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@"
+Map; 3900553433511235353736; dc01
+SwitchboardSensor; switchboardSensor_dc01; dc01
+Filter;switchboardSensor_dc01_rms; Min;600;switchboardSensor_dc01_rms
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("switchboardSensor_dc01_rms_filter").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("dc01_state", boardStateNames.First());
+        }
+
+        [TestMethod]
+        public void GetBoardStateNames_SwitchboardSensorExpandedInputField_Math()
+        {
+            // Arrange
+            var ioconf = new IOconfFile(@"
+Map; 3900553433511235353736; dc01
+SwitchboardSensor; switchboardSensor_dc01; dc01
+Math; switchboardSensor_dc01_rms_math; switchboardSensor_dc01_rms + 1
+".SplitNewLine(StringSplitOptions.None));
+
+            // Act
+            var boardStateNames = ioconf.GetBoardStateNames("switchboardSensor_dc01_rms_math").ToList();
+
+            // Assert
+            Assert.AreEqual(1, boardStateNames.Count());
+            Assert.AreEqual("dc01_state", boardStateNames.First());
         }
     }
 }
