32a07221dd680a95c61304db017a1e0fa9d219c7
LarsWithCA <lars.with@copenhagenatomics.com>
2024-08-01 11:23:03 +0200

    Extracted method for creating and starting shell process.

--- CA_DataUploaderLib/Helpers/DULutil.cs
+++ CA_DataUploaderLib/Helpers/DULutil.cs
@@ -37,32 +37,35 @@ namespace CA_DataUploaderLib.Helpers
 
         public static string ExecuteShellCommand(string command, int waitForExit = 1000)
         {
-            // execute shell command:
-            var info = new ProcessStartInfo()
-            {
-                FileName = "/bin/bash",
-                Arguments = "-c \"" + command + "\"",
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            };
-
-            using (var p = Process.Start(info) ?? throw new InvalidOperationException($"failed to start process {command}"))
+            using (var p = CreateAndStartShellProcess(command) ?? throw new InvalidOperationException($"Failed to start process {command}"))
             {
                 string? err = null;
                 p.ErrorDataReceived += (sender, e) => err += e.Data;
                 string output = p.StandardOutput.ReadToEnd();
                 if (!p.WaitForExit(waitForExit))
-                    CALog.LogData(LogID.A, $"timed out waiting for command to exit: {command}");
+                    CALog.LogData(LogID.A, $"Timed out waiting for command to exit: {command}");
                 else
                     p.WaitForExit(); // make sure all async events are finished handling https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.waitforexit?view=net-5.0#System_Diagnostics_Process_WaitForExit_System_Int32_
                 if (!string.IsNullOrEmpty(err))
-                    CALog.LogData(LogID.A, $"error while running command {command} - {err}");
+                    CALog.LogData(LogID.A, $"Error while running command {command} - {err}");
 
                 p.ErrorDataReceived -= (sender, e) => err += e.Data;
                 return output;
             }
+        }
+
+        public static Process? CreateAndStartShellProcess(string command)
+        {
+            return Process.Start(new ProcessStartInfo()
+            {
+                FileName = "/bin/bash",
+                Arguments = "-c \"" + command + "\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                WorkingDirectory = Environment.CurrentDirectory
+            });
         }
     }
 }
