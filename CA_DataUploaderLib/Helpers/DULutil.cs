e24ec5cef7356663c4595deb977daae5c5e7aaa9
Lars Baunegaard With <132266664+LarsWithCA@users.noreply.github.com>
2024-08-26 09:08:11 +0200

    DULutil: added method for executing command and returning standard + error output and the exit code. (#331)
    * DULutil: added method for executing command and returning standard + error output and the exit code.
    
    * DULutil.ExecuteShellCommand: fixed eventhandler not getting unsubscribed.

--- CA_DataUploaderLib/Helpers/DULutil.cs
+++ CA_DataUploaderLib/Helpers/DULutil.cs
@@ -40,7 +40,7 @@ namespace CA_DataUploaderLib.Helpers
             using (var p = CreateAndStartShellProcess(command) ?? throw new InvalidOperationException($"Failed to start process {command}"))
             {
                 string? err = null;
-                p.ErrorDataReceived += (sender, e) => err += e.Data;
+                p.ErrorDataReceived += ErrorDataReceived;
                 string output = p.StandardOutput.ReadToEnd();
                 if (!p.WaitForExit(waitForExit))
                     CALog.LogData(LogID.A, $"Timed out waiting for command to exit: {command}");
@@ -49,8 +49,10 @@ namespace CA_DataUploaderLib.Helpers
                 if (!string.IsNullOrEmpty(err))
                     CALog.LogData(LogID.A, $"Error while running command {command} - {err}");
 
-                p.ErrorDataReceived -= (sender, e) => err += e.Data;
+                p.ErrorDataReceived -= ErrorDataReceived;
                 return output;
+
+                void ErrorDataReceived(object sender, DataReceivedEventArgs e) => err += e.Data;
             }
         }
 
@@ -66,6 +68,58 @@ namespace CA_DataUploaderLib.Helpers
                 RedirectStandardOutput = true,
                 WorkingDirectory = Environment.CurrentDirectory
             });
+        }
+
+        public static (string stdOutput, string errOutput, int exitCode) ExecuteCommand(string command, string arguments, bool debug = false, Action<string>? logger = null)
+        {
+            var log = logger ?? (s => CALog.LogData(LogID.A, s));
+
+            if (debug)
+            {
+                log("");
+                log(@"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+                log($"Running command: {command} {arguments}");
+            }
+            string? errorOutput = string.Empty;
+            var p = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = Environment.CurrentDirectory
+                }
+            };
+            p.ErrorDataReceived += (sender, e) => { errorOutput += e.Data; };
+            if (!p.Start())
+                throw new Exception($"Unable to start process with command: {command} {arguments}");
+
+            // To avoid deadlocks, always read the output stream first and then wait.
+            // And, if you read both output and error stream use an asynchronous read operation on at least one of them.
+            p.BeginErrorReadLine();
+            string standardOutput = p.StandardOutput.ReadToEnd().TrimEnd();
+
+            if (!p.WaitForExit(60000))
+                throw new Exception($"Timed out waiting for command to exit: {command} {arguments}");
+
+            if (debug)
+            {
+                log($"ExitCode: {p.ExitCode}");
+                log("-----------");
+                if (!string.IsNullOrWhiteSpace(standardOutput))
+                    log(standardOutput);
+                log("-----------");
+                if (!string.IsNullOrWhiteSpace(errorOutput))
+                    log(errorOutput);
+                log(@"//////////////////////////////");
+                log("");
+            }
+
+            return (standardOutput, errorOutput, p.ExitCode);
         }
     }
 }
