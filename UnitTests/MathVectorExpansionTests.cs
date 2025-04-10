387a7c9db7db0f17e51a05c66d12b3fdb2eb1667
freddyrios <freddy.rios@copenhagenatomics.com>
2025-04-10 14:36:49 +0200

    fixed math expansion tests due to delayed initialization for tags expansion
    removed the 2 Ignores tests as those are more underlying details not exposed via the high level apis

--- UnitTests/MathVectorExpansionTests.cs
+++ UnitTests/MathVectorExpansionTests.cs
@@ -14,65 +14,52 @@ namespace UnitTests
         [TestMethod]
         public void VectorDescriptionIncludesMath()
         {
-            var math = GetNewMathExpansion(new("Math;MyMath;MyName + 2", 2));
-            Assert.AreEqual("MyMath", string.Join(Environment.NewLine, math.GetVectorDescriptionEntries().Select(e => e.Descriptor)));
+            var (cmd, desc, conf) = NewConf("Math;MyMath;MyName + 2", "Math;MyName;0");
+            Assert.AreEqual("MyMath,MyName", string.Join(',', desc._items.Select(e => e.Descriptor)));
         }
 
         [TestMethod]
         public void ExpandsVector()
         {
-            var math = GetInitializedMathExpansion(new("Math;MyMath;MyName + 2", 2), ["MyName", "MyMath"]);
-            var values = new[]{0.2, 0};
-            math.Apply(new DataVector(values, DateTime.UtcNow));
-            CollectionAssert.AreEqual(new[] { 0.2, 2.2 }, values);
+            var (cmd, desc, _) = NewConf("Math;MyName;0.2", "Math;MyMath;MyName + 2");
+            var vector = new DataVector(new double[desc.Length], DateTime.UtcNow);
+            cmd.MakeDecision([], DateTime.UtcNow.AddMilliseconds(100), ref vector, []);
+            CollectionAssert.AreEqual(new[] {0.2,2.2}, vector.Data);
         }
 
         [TestMethod]
         public void MathDependingOnMathUsesLatestValue()
         {
-            var math = new MathVectorExpansion([new IOconfMath("Math;Math1;Math1 + 1", 0), new("Math;Math2;Math1", 0)]);
-            math.Initialize(new[] { "Math1", "Math2" });
-            var values = new[] { 2d, 2 };
-            math.Apply(new DataVector(values, DateTime.UtcNow));
-            CollectionAssert.AreEqual(new[] { 3d, 3 }, values);
-        }
-
-        [TestMethod]
-        public void IgnoresUnusedValues()
-        {
-            var math = GetInitializedMathExpansion(new("Math;MyMath;MyName + 2", 2), ["MyName", "UnusedValue", "MyMath"]);
-            var values = new[]{0.2, 3, 0};
-            math.Apply(new DataVector(values, DateTime.UtcNow));
-            CollectionAssert.AreEqual(new[] { 0.2, 3, 2.2 }, values);
-        }
-
-        [TestMethod]
-        public void IgnoresIOConfMathChanges()
-        {
-            var ioconfEntries = new IOconfMath[] { new("Math;MyMath;MyName + 2", 2) };
-            var math = new MathVectorExpansion(ioconfEntries);
-            math.Initialize(new[] { "MyName", "MyMath", "MyOutput" });
-            var values = new[]{0.2,0,100};
-
-            ioconfEntries = new IOconfMath[] { new("Math;MyMath;MyName + 2", 2), new("Math;MyMath2;MyName + 3", 2) };
-            math.Apply(new DataVector(values, DateTime.UtcNow));
-
-            CollectionAssert.AreEqual(new[] { 0.2, 2.2, 100 }, values, "the extra math statement must be ignored and must not incorrectly update the output");
+            var (cmd, desc, _) = NewConf("Math;Math1;Math1 + 1", "Math;Math2;Math1");
+            var vector = new DataVector(new double[desc.Length], DateTime.UtcNow);
+            vector.Data[0] = 2d; vector.Data[1] = 2;
+            cmd.MakeDecision([], DateTime.UtcNow.AddMilliseconds(100), ref vector, []);
+            CollectionAssert.AreEqual(new[] { 3d, 3 }, vector.Data);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "If the Math-expression references a source which is not in the vector, this should cause an exception.")]
         public void MathReferencingSourceNotInVectorCausesException()
         {
-            GetInitializedMathExpansion(new("Math;MyMath;NotInVector + 2", 2), ["MyMath"]);
+            _ = NewConf("Math;MyMath;NotInVector + 2");
         }
 
         [TestMethod]
         public async Task CanExpandVectorInParallel()
         {
-            var math = GetInitializedMathExpansion(
-                new("Math;MyMath;f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10", 0), 
-                ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "MyMath"]);
+            var (cmd, desc, _) = NewConf(
+                "Math;f1;f1",
+                "Math;f2;f2",
+                "Math;f3;f3",
+                "Math;f4;f4",
+                "Math;f5;f5",
+                "Math;f6;f6",
+                "Math;f7;f7",
+                "Math;f8;f8",
+                "Math;f9;f9",
+                "Math;f10;f10",
+                "Math;MyMath;f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10"
+                );
             await ParallelHelper.ForRacingThreads(0, 100, i =>
             {
                 var values = new double[11];
@@ -81,8 +68,9 @@ namespace UnitTests
                 var expected = values.Take(10).Sum();
                 return () =>
                 {
-                    math.Apply(new DataVector(values, DateTime.UtcNow));
-                    Assert.AreEqual(expected, values[10], "detected thread safety issue");
+                    var vector = new DataVector(values, DateTime.UtcNow);
+                    cmd.MakeDecision([], DateTime.UtcNow, ref vector, []);
+                    Assert.AreEqual(expected, vector.Data[10], "detected thread safety issue");
                 };
             });
         }
@@ -94,5 +82,12 @@ namespace UnitTests
             return expansion;
         }
         public static MathVectorExpansion GetNewMathExpansion(IOconfMath math) => new([math]);
+        private static (CommandHandler cmd, VectorDescription desc, IOconfFile conf) NewConf(params string[] lines)
+        {
+            var conf = new IOconfFile([.. lines]);
+            var cmd = new CommandHandler(conf);
+            var desc = cmd.GetFullSystemVectorDescription();
+            return (cmd, desc, conf);
+        }
     }
 }
