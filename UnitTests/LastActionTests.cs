8b9754f9d3cec821ddd814738295de1b5fefa0bf
LarsWithCA <lars.with@copenhagenatomics.com>
2025-05-08 14:36:10 +0200

    updated tests

--- UnitTests/LastActionTests.cs
+++ UnitTests/LastActionTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Time.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -16,11 +17,13 @@ namespace UnitTests
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.0, 0.1, 1.0, 1.0], now), "expiration is expected before executing the first action");
             now = now.AddSeconds(1);
             action.ExecutedNewAction([0.0, 0.0, 0.2, 1.0, 1.0], now);
+            CollectionAssert.AreEqual(new double[] { 0.2 }, action.Targets.ToArray());
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.0, 0.2, 1.0, 1.0], now), "2: no expiration expected at 0s");
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.0, 0.2, 1.0, 1.0], now.AddMilliseconds(500).AddTicks(-1)), "2: no expiration expected at 0.5s - 1 tick");
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.0, 0.2, 1.0, 1.0], now.AddMilliseconds(500)), "2: expiration expected at 0.5s");
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.0, 0.3, 1.0, 1.0], now.AddMilliseconds(100)), "2: target change must be detected");
             action.ExecutedNewAction([0.0, 0.0, 0.3, 0.0, 0.0], now);
+            CollectionAssert.AreEqual(new double[] { 0.3 }, action.Targets.ToArray());
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.0, 0.3, 1.0, 1.0], now), "3: no expiration expected at 0s");
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.0, 0.3, 1.0, 1.0], now.AddMilliseconds(500).AddTicks(-1)), "3: no expiration expected at 0.5s - 1 tick");
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.0, 0.3, 1.0, 1.0], now.AddMilliseconds(500)), "3: expiration expected at 0.5s");
@@ -67,10 +70,12 @@ namespace UnitTests
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.1, 0.2, 0.3, 1.0], now), "expiration is expected before executing the first action");
             now = now.AddSeconds(1);
             action.ExecutedNewAction([0.0, 0.2, 0.3, 0.4, 1.0], now);
+            CollectionAssert.AreEqual(new double[] { 0.2, 0.3, 0.4 }, action.Targets.ToArray());
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.2, 0.3, 0.4, 1.0], now), "2: no expiration expected at 0s");
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.2, 0.3, 0.4, 1.0], now.AddYears(10)), "2: no expiration expected at 10 years");
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.3, 0.4, 0.5, 1.0], now.AddMilliseconds(100)), "2: target change must be detected");
             action.ExecutedNewAction([0.0, 0.3, 0.4, 0.5, 1.0], now);
+            CollectionAssert.AreEqual(new double[] { 0.3, 0.4, 0.5 }, action.Targets.ToArray());
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.3, 0.4, 0.5, 1.0], now), "3: no expiration expected at 0s");
             Assert.IsFalse(action.ChangedOrExpired([0.0, 0.3, 0.4, 0.5, 1.0], now.AddDays(30)), "3: no expiration expected at 30 days");
             Assert.IsTrue(action.ChangedOrExpired([0.0, 0.3, 0.2, 0.5, 1.0], now.AddMilliseconds(100)), "3: target change must be detected");
