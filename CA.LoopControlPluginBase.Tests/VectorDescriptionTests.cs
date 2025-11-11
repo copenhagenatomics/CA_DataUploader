8511732501d7d9338c02f82e7a556e9792515289
freddyrios <70888589+freddyrios@users.noreply.github.com>
2025-11-11 12:29:51 +0100

    added tests project for the plugins contract (#406)
    * added tests project for the plugins contract
    
    * changed plugins path match to also include the plugins tests

---
+++ CA.LoopControlPluginBase.Tests/VectorDescriptionTests.cs
@@ -0,0 +1,16 @@
+﻿namespace CA.LoopControlPluginBase.Tests
+{
+    [TestClass]
+    public sealed class VectorDescriptionTests
+    {
+        [TestMethod]
+        public void CanAccess3DescriptionFields()
+        {
+            var desc = new VectorDescription(["f1", "f2", "f3"]);
+            Assert.AreEqual(3, desc.Count);
+            Assert.AreEqual("f1", desc[0]);
+            Assert.AreEqual("f2", desc[1]);
+            Assert.AreEqual("f3", desc[2]);
+        }
+    }
+}
